1136a6ff9a49009a08a635d49f6ea53cb08d791b
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-05-04 19:15:12 +0000

    [+] Добавлен виджет `NetworkWidget` в класс `MainChannel`. [*] Изменены названия приватных мемберов в класс `MainChannel`. [*] Изменены параметры конструктора `MainChannel`.

--- trunk/src/schat/mainchannel.cpp
+++ trunk/src/schat/mainchannel.cpp
@@ -6,38 +6,52 @@
 #include <QtGui>
 
 #include "mainchannel.h"
+#include "networkwidget.h"
+#include "settings.h"
 
-MainChannel::MainChannel(const QString &server, QWidget *parent)
+MainChannel::MainChannel(Settings *settings, QWidget *parent)
   : AbstractTab(parent)
 {
   setAttribute(Qt::WA_DeleteOnClose);
   type = Main;
   
-  serverLabel = new QLabel(tr("Адрес сервера:"), this);
-  serverEdit  = new QLineEdit(server, this);
+  m_settings = settings;
+  
+  m_serverLabel = new QLabel(tr("Адрес сервера:"), this);
+  m_serverEdit  = new QLineEdit(m_settings->server, this);
   
   createActions();
   
-  serverLabel->setVisible(false);
-  serverEdit->setVisible(false);
-  connectCreateButton->setVisible(false);
+  m_serverLabel->setVisible(false);
+  m_serverEdit->setVisible(false);
+  m_connectCreateButton->setVisible(false);
   
-  topLayout = new QHBoxLayout;
-  topLayout->addWidget(serverLabel);
-  topLayout->addWidget(serverEdit);
-  topLayout->addWidget(connectCreateButton);
-  topLayout->addStretch();
-  topLayout->setMargin(0);
+  m_networkWidget = new NetworkWidget(&settings->networksModel, this);
+  m_networkWidget->setVisible(false);
   
-  mainLayout = new QVBoxLayout;
-  mainLayout->addLayout(topLayout);
-  mainLayout->addWidget(browser);
-  mainLayout->setMargin(0);
-  mainLayout->setSpacing(2);
-  setLayout(mainLayout);
+  m_topLayout = new QHBoxLayout;
+  m_topLayout->addWidget(m_serverLabel);
+  m_topLayout->addWidget(m_serverEdit);
+//  m_topLayout->addWidget(m_connectCreateButton);
+  m_topLayout->addStretch();
+  m_topLayout->setMargin(0);
   
-  connect(serverEdit, SIGNAL(returnPressed()), parent, SLOT(serverChanged()));
-  connect(connectCreateAction, SIGNAL(triggered()), parent, SLOT(serverChanged()));
+  m_networkLayout = new QHBoxLayout;
+  m_networkLayout->addWidget(m_networkWidget);
+  m_networkLayout->addWidget(m_connectCreateButton);
+  m_networkLayout->addStretch();
+  m_networkLayout->setMargin(0);
+  
+  m_mainLayout = new QVBoxLayout;
+  m_mainLayout->addLayout(m_networkLayout);
+  m_mainLayout->addLayout(m_topLayout);
+  m_mainLayout->addWidget(browser);
+  m_mainLayout->setMargin(0);
+  m_mainLayout->setSpacing(2);
+  setLayout(m_mainLayout);
+  
+  connect(m_serverEdit, SIGNAL(returnPressed()), parent, SLOT(serverChanged()));
+  connect(m_connectCreateAction, SIGNAL(triggered()), parent, SLOT(serverChanged()));
 }
 
 
@@ -47,16 +61,19 @@ MainChannel::MainChannel(const QString &server, QWidget *parent)
 void MainChannel::displayChoiceServer(bool display)
 {
   if (display) {
-    topLayout->setContentsMargins(4, 2, 4, 0);
-    serverLabel->setVisible(true);
-    serverEdit->setVisible(true);
-    connectCreateButton->setVisible(true);
+    m_topLayout->setContentsMargins(4, 2, 4, 0);
+    m_networkLayout->setContentsMargins(4, 2, 4, 0);
+    m_serverLabel->setVisible(true);
+    m_serverEdit->setVisible(true);
+    m_networkWidget->setVisible(true);
+    m_connectCreateButton->setVisible(true);
   }
   else {
-    serverLabel->setVisible(false);
-    serverEdit->setVisible(false);
-    connectCreateButton->setVisible(false);
-    topLayout->setMargin(0);
+    m_serverLabel->setVisible(false);
+    m_serverEdit->setVisible(false);
+    m_connectCreateButton->setVisible(false);
+    m_networkWidget->setVisible(false);
+    m_topLayout->setMargin(0);
   }
   browser->scroll();
 }
@@ -67,8 +84,8 @@ void MainChannel::displayChoiceServer(bool display)
  */
 void MainChannel::createActions()
 {
-  connectCreateButton = new QToolButton(this);
-  connectCreateAction = new QAction(QIcon(":/images/connect_creating.png"), tr("Подключится к серверу"), this);
-  connectCreateButton->setDefaultAction(connectCreateAction);
-  connectCreateButton->setAutoRaise(true);
+  m_connectCreateButton = new QToolButton(this);
+  m_connectCreateAction = new QAction(QIcon(":/images/connect_creating.png"), tr("Подключится к серверу"), this);
+  m_connectCreateButton->setDefaultAction(m_connectCreateAction);
+  m_connectCreateButton->setAutoRaise(true);
 }
