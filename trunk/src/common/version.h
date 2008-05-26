d6033474d889473ffc871bc813579366ff91e300
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-05-26 23:14:38 +0000

    [*] В файлах сети элемент <ip> переименован в <host>, добавлена возможность указывать порт например <host>192.168.5.130:7666</host>.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -6,7 +6,7 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.0.2.115 Alpha"
-#define SCHAT_VERSION_RC 0,0,2,115
+#define SCHAT_VERSION    "0.0.2.116 Alpha"
+#define SCHAT_VERSION_RC 0,0,2,116
 
 #endif /*VERSION_H_*/
