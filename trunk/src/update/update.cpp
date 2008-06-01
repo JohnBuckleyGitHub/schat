50a00ac0018ae21f27dc03b2e2157168c0f597a2
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-06-01 23:44:24 +0000

    [*] Оптимизация класса `Update` копия `schat-update.exe` теперь копируется в корень программы.

--- trunk/src/update/update.cpp
+++ trunk/src/update/update.cpp
@@ -16,7 +16,8 @@ Update::Update(const QUrl &url, QObject *parent)
   : QObject(parent)
 {
   m_url = url;
-  m_updatesPath = qApp->applicationDirPath() + "/updates";
+  m_appPath = qApp->applicationDirPath();
+  m_updatesPath = m_appPath + "/updates";
   m_download = new Download(this);
   m_download->setBasePath(m_updatesPath);
   
@@ -35,8 +36,8 @@ void Update::execute()
       qApp->exit(64); // не удалось создать папку для обновлений
   
   m_state = GettingUpdateXml;
-  m_download->get(QUrl(DefaultUpdateXmlUrl));
-  m_reader.setPath(QFileInfo(DefaultUpdateXmlUrl).path());
+  m_download->get(m_url);
+  m_reader.setPath(QFileInfo(m_url.toString()).path());
 }
 
 
@@ -68,21 +69,28 @@ void Update::saved(const QString &filename)
     
     m_state = GettingUpdates;
     m_queue = m_reader.queue();
+    
+    foreach (QString url, m_queue)
+      m_files << QFileInfo(url).fileName();
+    
     m_download->get(QUrl(m_queue.dequeue()));
   }
   else if (m_state == GettingUpdates) {
     if (m_queue.isEmpty()) {
-      QString newName = qApp->applicationDirPath() + "/updates/schat-install.exe";
+      QString newName = m_appPath + "/schat-install.exe";
       
       if (QFile::exists(newName))
         QFile::remove(newName);
       
-      if (!QFile::copy(qApp->applicationDirPath() + "/schat-update.exe", newName)) {
+      if (!QFile::copy(m_appPath + "/schat-update.exe", newName)) {
         qApp->exit(4); // Ошибка создания копии
         return;
       }
-        
+      
+//      QProcess::startDetached(m_appPath + "/schat-install.exe", QStringList() << "-install", m_appPath);
+         
       qApp->exit(0); // обновления успешно скачаны
+
     }
     else
       m_download->get(QUrl(m_queue.dequeue()));
