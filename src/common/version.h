14f995bdbb005c6c68af1b077fb180e0dba83664
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-06-13 14:30:37 +0000

    [*] Исправлен запуск инсталляторов.

--- src/common/version.h
+++ src/common/version.h
@@ -6,8 +6,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.0.3.150 Alpha"
-#define SCHAT_VERSION_RC 0,0,3,150
+#define SCHAT_VERSION    "0.0.3.151 Alpha"
+#define SCHAT_VERSION_RC 0,0,3,151
 
 static const int UpdateLevelQt   = 0;
 static const int UpdateLevelCore = 0;
--- src/update/install.cpp
+++ src/update/install.cpp
@@ -14,7 +14,7 @@
 Install::Install(QObject *parent)
   : QObject(parent)
 {
-  m_appPath = qApp->applicationDirPath();
+  m_appPath = QDir::toNativeSeparators(qApp->applicationDirPath());
   m_s = new QSettings(m_appPath + "/schat.conf", QSettings::IniFormat, this);
   m_ready = m_s->value("Updates/ReadyToInstall", false).toBool();
   QStringList list = m_s->value("Updates/Files", QStringList()).toStringList();
@@ -38,7 +38,7 @@ void Install::execute()
     fail();
   
   if (!m_queue.isEmpty())
-    m_process.start(m_appPath + "/updates/" + m_queue.dequeue(), QStringList() << "/S" << ("/D=" + m_appPath));
+    m_process.start('"' + m_appPath + "/updates/" + m_queue.dequeue() + '"', QStringList() << "/S" << ("/D=" + m_appPath));
 }
 
 
