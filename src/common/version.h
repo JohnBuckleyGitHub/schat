252faffa4774c8016e0717bd9742e5c1104e3e80
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-06-19 21:35:21 +0000

    [*] Исправлена ошибка с некорректной передачей ключа "/D=" если путь содержал пробелы.

--- src/common/version.h
+++ src/common/version.h
@@ -6,10 +6,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.0.3.159 Alpha"
-#define SCHAT_VERSION_RC 0,0,3,159
+#define SCHAT_VERSION    "0.0.3.160 Alpha"
+#define SCHAT_VERSION_RC 0,0,3,160
 
 static const int UpdateLevelQt   = 1;
-static const int UpdateLevelCore = 2;
+static const int UpdateLevelCore = 3;
 
 #endif /*VERSION_H_*/
