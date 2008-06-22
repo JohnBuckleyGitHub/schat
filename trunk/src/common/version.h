4142968a9d016a86a10d060c1ca553486422cf1f
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-06-22 17:08:34 +0000

    [*] Файлы класса `UpdateNotify` перенесены в папку `win32`.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -6,10 +6,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.0.3.163 Alpha"
-#define SCHAT_VERSION_RC 0,0,3,163
+#define SCHAT_VERSION    "0.0.3.164 Alpha"
+#define SCHAT_VERSION_RC 0,0,3,164
 
 static const int UpdateLevelQt   = 1;
-static const int UpdateLevelCore = 6;
+static const int UpdateLevelCore = 7;
 
 #endif /*VERSION_H_*/
