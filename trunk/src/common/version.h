7ad88f02472a1083d05f9c5e8c91fb77465cbf74
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-06-23 21:07:03 +0000

    [*] Изменён логотип программы.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -6,10 +6,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.0.3.167 Alpha"
-#define SCHAT_VERSION_RC 0,0,3,167
+#define SCHAT_VERSION    "0.0.3.168 Alpha"
+#define SCHAT_VERSION_RC 0,0,3,168
 
 static const int UpdateLevelQt   = 1;
-static const int UpdateLevelCore = 10;
+static const int UpdateLevelCore = 11;
 
 #endif /*VERSION_H_*/
