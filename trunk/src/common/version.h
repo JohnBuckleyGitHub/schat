be3a2d2637d75d44e2a834c4923e4fcda41b9770
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-06-27 16:21:45 +0000

    [+] Добавлена сеть "Achim Network (Internet)".

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -6,10 +6,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.0.3.170 Alpha"
+#define SCHAT_VERSION    "0.0.3.171 Alpha"
 #define SCHAT_VERSION_RC 0,0,3,170
 
 static const int UpdateLevelQt   = 1;
-static const int UpdateLevelCore = 12;
+static const int UpdateLevelCore = 13;
 
 #endif /*VERSION_H_*/
