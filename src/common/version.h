5db8857fc00675202c613f8b1d2526aa54c65be9
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-06-28 11:08:13 +0000

    [+] Добавлен базовый каркас класса `Log`.

--- src/common/version.h
+++ src/common/version.h
@@ -6,8 +6,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.0.4.177 Alpha"
-#define SCHAT_VERSION_RC 0,0,4,177
+#define SCHAT_VERSION    "0.0.4.178 Alpha"
+#define SCHAT_VERSION_RC 0,0,4,178
 
 static const int UpdateLevelQt   = 2;
 static const int UpdateLevelCore = 16;
