91949ceff1101cd6fb9dc5488d22657e72cb6125
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-07-14 08:56:40 +0000

    [*] Для ников содержащих не допустимые для имён файлов символы теперь файлы журнала корректно сохраняются.

--- src/common/profile.cpp
+++ src/common/profile.cpp
@@ -1,6 +1,19 @@
 /* $Id$
- * Simple Chat
+ * IMPOMEZIA Simple Chat
  * Copyright © 2008 IMPOMEZIA (http://impomezia.net.ru)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <QtCore>
@@ -114,11 +127,7 @@ bool Profile::isValidNick(const QString &n)
     return false;
   if (nick.startsWith(QChar('#')))
     return false;
-  else if (nick == "COM1" || nick == "COM2" || nick == "COM3" || nick == "COM4" || nick == "COM5" || nick == "COM6" || nick == "COM7" || nick == "COM8" || nick == "COM9" || nick == "AUX")
-    return false;
-  else if (nick == "LPT1" || nick == "LPT2" || nick == "LPT3" || nick == "LPT4" || nick == "LPT5" || nick == "LPT6" || nick == "LPT7" || nick == "LPT8" || nick == "LPT9" || nick == "PRN")
-    return false;
-  else if (nick == "CON" || nick == "NUL")
+  else if (nick == ".")
     return false;
   
   return true;
