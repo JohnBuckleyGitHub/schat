bfcac9fd0f2b21f6960937aa8b54e28e10c24e2e
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-07-29 14:09:28 +0000

    [*] Все изменения из проекта "daemon" перенесены в "schatd". [-] Проект "daemon" удалён.

---
+++ trunk/src/schatd/daemon.h
@@ -0,0 +1,68 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008 IMPOMEZIA (http://impomezia.net.ru)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DAEMON_H_
+#define DAEMON_H_
+
+#include <QTcpServer>
+#include <QHash>
+
+class ChannelLog;
+class DaemonService;
+class DaemonSettings;
+class UserUnit;
+
+class Daemon : public QObject
+{
+  Q_OBJECT
+
+public:
+  Daemon(QObject *parent = 0);
+  bool start();
+  
+signals:
+  void message(const QString &sender, const QString &message);
+  void newUser(const QStringList &list, bool echo);
+  void sendNewNick(quint8 gender, const QString &nick, const QString &newNick, const QString &name);
+  void sendNewProfile(quint8 gender, const QString &nick, const QString &name);
+  void userLeave(const QString &nick, const QString &bye, bool echo);
+
+public slots:
+  void incomingConnection();
+  
+private slots:
+  void greeting(const QStringList &list);
+  void message(const QString &channel, const QString &sender, const QString &message);
+  void newBye(const QString &nick, const QString &bye);
+  void newNick(quint8 gender, const QString &nick, const QString &newNick, const QString &name);
+  void newProfile(quint8 gender, const QString &nick, const QString &name);
+  void userLeave(const QString &nick);
+
+private:
+  bool parseCmd(const QString &nick, const QString &msg);
+  QString serverInfo() const;
+  void sendAllUsers(DaemonService *service);
+  
+  ChannelLog *m_channelLog;
+  ChannelLog *m_privateLog;
+  DaemonSettings *m_settings;
+  QHash<QString, UserUnit *> m_users;
+  QTcpServer m_server;
+};
+
+#endif /*DAEMON_H_*/
--- trunk/src/schatd/schatd.pro
+++ trunk/src/schatd/schatd.pro
@@ -1,34 +1,51 @@
 # $Id$
-# Simple Chat
+# IMPOMEZIA Simple Chat
 # Copyright © 2008 IMPOMEZIA (http://impomezia.net.ru)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 TEMPLATE = app
 QT += network
-CONFIG += console
+CONFIG += console warn_on
 HEADERS = \
+    abstractprofile.h \
     channellog.h \
+    daemon.h \
+    daemonservice.h \
     daemonsettings.h \
     log.h \
     profile.h \
     protocol.h \
-    server.h \
-    serversocket.h \
-    version.h
+    userunit.h \
+    version.h \
     
 SOURCES = \
+    abstractprofile.cpp \
     channellog.cpp \
+    daemon.cpp \
+    daemonservice.cpp \
     daemonsettings.cpp \
     log.cpp \
     main.cpp \
     profile.cpp \
-    server.cpp \
-    serversocket.cpp
+    userunit.cpp \
     
 DEPENDPATH  += . \
-               ../common
+               ../common \
                 
 INCLUDEPATH += . \
-               ../common
+               ../common \
 
 CONFIG(debug, debug|release) { 
     RCC_DIR = ../../tmp/schatd/debug/rcc
