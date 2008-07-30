3f79190fae86fdde4575c4b93347c58cbbf48d8c
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-07-30 17:53:33 +0000

    [+] В класс NetworkReader добавлена поддержка элемента <key>.

--- trunk/src/schat/networkreader.h
+++ trunk/src/schat/networkreader.h
@@ -1,6 +1,19 @@
 /* $Id$
- * Simple Chat
+ * IMPOMEZIA Simple Chat
  * Copyright © 2008 IMPOMEZIA (http://impomezia.net.ru)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef NETWORKREADER_H_
@@ -17,21 +30,22 @@ class NetworkReader : public QXmlStreamReader {
   
 public:
   NetworkReader();
-  
   bool readFile(const QString &fileName);
+  inline QList<ServerInfo> servers() const { return m_servers; }
   inline QString description() const       { return m_description; }
+  inline QString key() const               { return m_key; }
   inline QString networkName() const       { return m_name; }
   inline QString site() const              { return m_site; }
-  inline QList<ServerInfo> servers() const { return m_servers; }
-  
+
 private:
   void readMeta();
   void readNetwork();
   void readServers();
   void readUnknownElement();
-  
+
   QList<ServerInfo> m_servers;
   QString m_description;
+  QString m_key;
   QString m_name;
   QString m_site;
 };
