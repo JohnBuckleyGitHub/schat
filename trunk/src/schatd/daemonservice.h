a8166d5ea55a8031ad205b9f0368c955989bd397
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-08-03 13:29:55 +0000

    [+] Реализовано удаление линков из хеша `m_links`.

--- trunk/src/schatd/daemonservice.h
+++ trunk/src/schatd/daemonservice.h
@@ -41,7 +41,7 @@ public:
 
 signals:
   void greeting(const QStringList &list, quint8 flag);
-  void leave(const QString &nick);
+  void leave(const QString &nick, quint8 flag);
   void message(const QString &channel, const QString &sender, const QString &message);
   void newBye(const QString &nick, const QString &bye);
   void newNick(quint8 gender, const QString &nick, const QString &newNick, const QString &name);
