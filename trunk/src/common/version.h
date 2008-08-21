4138f343283deeec751a0bd5db63142b6010b92a
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-08-21 16:48:40 +0000

    [*] Подготовка к релизу 0.1.0.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.1.0.404 TRUNK"
-#define SCHAT_VERSION_RC 0,1,0,404
+#define SCHAT_VERSION    "0.1.0.407 Alpha"
+#define SCHAT_VERSION_RC 0,1,0,407
 
 static const int UpdateLevelQt   = 4;
-static const int UpdateLevelCore = 42;
+static const int UpdateLevelCore = 43;
 
 #endif /*VERSION_H_*/
