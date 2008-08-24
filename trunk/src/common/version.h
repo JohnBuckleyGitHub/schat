685dff05689115bfc11a6d5855c5b4d67a6882df
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-08-24 19:31:48 +0000

    [*] Промежуточный релиз.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.2.0.430 TRUNK"
-#define SCHAT_VERSION_RC 0,2,0,430
+#define SCHAT_VERSION    "0.2.0.437 TRUNK"
+#define SCHAT_VERSION_RC 0,2,0,437
 
 static const int UpdateLevelQt   = 5;
-static const int UpdateLevelCore = 20003;
+static const int UpdateLevelCore = 20004;
 
 #endif /*VERSION_H_*/
