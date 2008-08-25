946d590270aeda1dc7ab5df4bb0fc9fbc6ca67ee
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-08-25 22:55:47 +0000

    [*] Изменён формат хранения имён файлов смайликов.

--- trunk/src/schat/settings.cpp
+++ trunk/src/schat/settings.cpp
@@ -41,8 +41,8 @@ Settings::Settings(const QString &filename, AbstractProfile *profile, QObject *p
 
 bool Settings::insertSmile(QTextCursor &cursor, const QString &smile)
 {
-  if (m_emoticons.contains(smile))
-    cursor.insertImage(m_emoticons.value(smile));
+//  if (m_emoticons.contains(smile))
+//    cursor.insertImage(m_emoticons.value(smile));
 
   return true;
 }
@@ -50,7 +50,7 @@ bool Settings::insertSmile(QTextCursor &cursor, const QString &smile)
 
 QString Settings::nextSmile(const QString &text, int pos) const
 {
-  QMapIterator <QString, QString> i(m_emoticons);
+  QMapIterator <QString, int> i(m_emoticons);
   while (i.hasNext()) {
     i.next();
     QString key = i.key();
@@ -66,8 +66,8 @@ QString Settings::nextSmile(const QString &text, int pos) const
 void Settings::createEmoticonsMap()
 {
   m_emoticons.clear();
-  m_emoticons.insert(":)", ":/images/logo16.png");
-  m_emoticons.insert("ПЫЩ", ":/images/quit.png");
+//  m_emoticons.insert(":)", ":/images/logo16.png");
+//  m_emoticons.insert("ПЫЩ", ":/images/quit.png");
 }
 
 
@@ -101,6 +101,7 @@ void Settings::read()
   readBool("HideWelcome", false);
   readBool("FirstRun", true);
   readString("Style", "Plastique");
+  readString("EmoticonsPath", qApp->applicationDirPath() + "/emoticons/kolobok");
   qApp->setStyle(getString("Style"));
 
   network.fromConfig(m_settings->value("Network", "AchimNet.xml").toString());
