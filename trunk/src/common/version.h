bda2f6ce451dc6d0adc5fea9b1e7adde4876d97e
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-08-26 14:40:22 +0000

    [+] Изменён инсталлятор для поддержки смайликов.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.2.0.437 TRUNK"
-#define SCHAT_VERSION_RC 0,2,0,437
+#define SCHAT_VERSION    "0.2.0.454 TRUNK"
+#define SCHAT_VERSION_RC 0,2,0,454
 
-static const int UpdateLevelQt   = 5;
-static const int UpdateLevelCore = 20004;
+static const int UpdateLevelQt   = 20000454;
+static const int UpdateLevelCore = 20000454;
 
 #endif /*VERSION_H_*/
