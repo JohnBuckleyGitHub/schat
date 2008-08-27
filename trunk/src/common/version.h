1bf70c11bcbbc0ad7292d756076ce2c8cdb87e56
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-08-27 17:53:23 +0000

    [*] Устранена ошибка.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.2.0.469 TRUNK"
-#define SCHAT_VERSION_RC 0,2,0,469
+#define SCHAT_VERSION    "0.2.0.470 TRUNK"
+#define SCHAT_VERSION_RC 0,2,0,470
 
 static const int UpdateLevelQt   = 20000454;
-static const int UpdateLevelCore = 20000469;
+static const int UpdateLevelCore = 20000470;
 
 #endif /*VERSION_H_*/
