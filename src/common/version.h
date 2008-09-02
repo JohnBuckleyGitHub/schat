25c758832337723690848911e4da2b96fa26945b
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-09-02 22:55:05 +0000

    [*] Реализована фильтрация сообщений на стороне сервера.

--- src/common/version.h
+++ src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.2.0.505 TRUNK"
-#define SCHAT_VERSION_RC 0,2,0,505
+#define SCHAT_VERSION    "0.2.0.507 TRUNK"
+#define SCHAT_VERSION_RC 0,2,0,507
 
 static const int UpdateLevelQt   = 20000454;
-static const int UpdateLevelCore = 20000505;
+static const int UpdateLevelCore = 20000507;
 
 #endif /*VERSION_H_*/
