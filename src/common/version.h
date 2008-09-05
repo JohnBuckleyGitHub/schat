25c9cd003cc1fb8bacb8cb437e5e2c7b4cfbe5d6
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-09-05 21:47:02 +0000

    [*] Релиз 0.2.0.

--- src/common/version.h
+++ src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.2.0.515 TRUNK"
-#define SCHAT_VERSION_RC 0,2,0,515
+#define SCHAT_VERSION    "0.2.0.516 Alpha"
+#define SCHAT_VERSION_RC 0,2,0,516
 
-static const int UpdateLevelQt   = 20000454;
-static const int UpdateLevelCore = 20000515;
+static const int UpdateLevelQt   = 20000516;
+static const int UpdateLevelCore = 20000516;
 
 #endif /*VERSION_H_*/
