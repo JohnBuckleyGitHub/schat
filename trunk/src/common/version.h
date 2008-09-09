cb8505ac885a75a5b5e37fd805477d9548c092c1
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-09-09 16:50:24 +0000

    [*] Исправлено некорректное, в некоторых случаях, обновление кнопок, при выделении справа налево в строке ввода.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.3.0.528 TRUNK"
-#define SCHAT_VERSION_RC 0,3,0,528
+#define SCHAT_VERSION    "0.3.0.531 TRUNK"
+#define SCHAT_VERSION_RC 0,3,0,531
 
 static const int UpdateLevelQt   = 20000516;
-static const int UpdateLevelCore = 30000528;
+static const int UpdateLevelCore = 2008090900;
 
 #endif /*VERSION_H_*/
--- trunk/src/schat/widget/sendwidget.h
+++ trunk/src/schat/widget/sendwidget.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008 IMPOMEZIA (http://impomezia.net.ru)
+ * Copyright © 2008 IMPOMEZIA (http://impomezia.com)
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -46,7 +46,7 @@ public slots:
   void copy() { m_input->copy(); }
 
 private slots:
-  void currentCharFormatChanged(const QTextCharFormat &format);
+  void cursorPositionChanged();
   void setBold(bool b);
   void setItalic(bool b);
   void setSettings();
