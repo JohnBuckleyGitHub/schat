578fc5dd53c9f4a89e5e2147ad31d0bea7f0db07
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-09-14 13:53:29 +0000

    [+] [schatd] Базовый каркас класса `LocalService`.

---
+++ src/schatd/ipc/localservice.cpp
@@ -0,0 +1,45 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008 IMPOMEZIA (http://impomezia.com)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtCore>
+#include <QtNetwork>
+
+#include "ipc/localservice.h"
+
+/*!
+ * \class LocalService
+ * \brief Класс, обслуживающий локальные соединения.
+ * 
+ * При наличии валидного сокета, инициализируется сокет.
+ */
+
+/*!
+ * \brief Конструктор класса LocalService.
+ */
+LocalService::LocalService(QLocalSocket *socket, QObject *parent)
+  : QObject(parent), m_socket(socket)
+{
+  qDebug() << "LocalService::LocalService()";
+
+  if (m_socket) {
+//    connect(m_socket, SIGNAL(readyRead()), SLOT(readyRead()));
+//    connect(m_socket, SIGNAL(disconnected()), SLOT(disconnected()));
+  }
+  else
+    deleteLater();
+}
