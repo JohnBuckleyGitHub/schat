9cab0e42207393b79cb8c7e9ff8ed39d03733a88
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-10-02 12:16:07 +0000

    [*] [schatd] Некоторые публичные слоты сделаны приватными.

--- trunk/src/schatd/daemonservice.h
+++ trunk/src/schatd/daemonservice.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008 IMPOMEZIA (http://impomezia.net.ru)
+ * Copyright © 2008 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -59,15 +59,15 @@ public slots:
   inline void sendNewLink(quint8 numeric, const QString &network, const QString &ip)       { send(OpcodeNewLink, numeric, network, ip); }
   inline void sendSyncBye(const QString &nick, const QString &bye)                         { send(OpcodeSyncByeMsg, nick, bye); }
   inline void sendUserLeave(const QString &nick, const QString &bye, quint8 flag)          { send(OpcodeUserLeave, flag, nick, bye); }
-  void disconnected();
-  void readyRead();
   void sendNewNick(quint8 gender, const QString &nick, const QString &newNick, const QString &name);
   void sendNewProfile(quint8 gender, const QString &nick, const QString &name);
   void sendNewUser(const QStringList &list, quint8 echo = 1, quint8 numeric = 0);
   void sendRelayMessage(const QString &channel, const QString &sender, const QString &message);
 
 private slots:
+  void disconnected();
   void ping();
+  void readyRead();
 
 private:
   bool opcodeGreeting();
@@ -104,7 +104,7 @@ private:
 
 /*! \fn void DaemonService::sendPrivateMessage(quint8 flag, const QString &nick, const QString &message)
  * \brief Отправка пакета \b OpcodePrivateMessage.
- * 
+ *
  * \param flag Флаг эха, если 1 то это подтверждение отправки сообщения.
  * \param nick Ник, отправившего сообщение (flag = 0), ник того кому предназначается сообщение (flag = 1).
  * \param message Сообщение.
@@ -112,7 +112,7 @@ private:
 
 /*! \fn void DaemonService::message(const QString &channel, const QString &sender, const QString &message)
  * \brief Уведомление о новом сообщении от пользователя.
- * 
+ *
  * \param channel Канал/ник для кого предназначено сообщение (пустая строка - главный канал).
  * \param sender Ник отправителя сообщения.
  * \param message Сообщение.
@@ -120,7 +120,7 @@ private:
 
 /*! \fn void DaemonService::sendLinkLeave(quint8 numeric, const QString &network, const QString &ip)
  * \brief Отправка пакета \b OpcodeLinkLeave.
- * 
+ *
  * \param numeric Номер сервера подключившегося к сети.
  * \param network Название сети.
  * \param ip Адрес сервера
@@ -128,7 +128,7 @@ private:
 
 /*! \fn void DaemonService::sendNewLink(quint8 numeric, const QString &network, const QString &ip)
  * \brief Отправка пакета \b OpcodeNewLink.
- * 
+ *
  * \param numeric Номер сервера подключившегося к сети.
  * \param network Название сети.
  * \param ip Адрес сервера
