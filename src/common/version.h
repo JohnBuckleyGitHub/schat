db77b1e8d86552fcbe2157d5e93fe5769fd5c878
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-10-07 21:36:46 +0000

    [+] [schatd-ui] Добавлена задержка ожидания отключения при "Выход с остановкой сервера".

--- src/common/version.h
+++ src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.3.0.670 TRUNK"
-#define SCHAT_VERSION_RC 0,3,0,670
+#define SCHAT_VERSION    "0.3.0.671 TRUNK"
+#define SCHAT_VERSION_RC 0,3,0,671
 
 static const int UpdateLevelQt   = 2008093000;
-static const int UpdateLevelCore = 2008100702;
+static const int UpdateLevelCore = 2008100800;
 
 #endif /*VERSION_H_*/
