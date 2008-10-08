4622eb1308f03e3dbc9dbfb7751a97633cb99f25
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-10-08 20:36:14 +0000

    [*] Начало разработки ветки 0.4.0.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -19,10 +19,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION    "0.3.0.674 Alpha"
-#define SCHAT_VERSION_RC 0,3,0,674
+#define SCHAT_VERSION    "0.4.0.678 TRUNK"
+#define SCHAT_VERSION_RC 0,4,0,678
 
 static const int UpdateLevelQt   = 2008100801;
-static const int UpdateLevelCore = 2008100801;
+static const int UpdateLevelCore = 2008100900;
 
 #endif /*VERSION_H_*/
