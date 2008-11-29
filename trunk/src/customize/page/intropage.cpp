6aa2b1f7624db6aad53d8770398d6c7578826ed6
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-11-29 13:09:00 +0000

    [*] [customize] Перенос файлов класса `IntroPage` [2/2].

---
+++ trunk/src/customize/page/intropage.cpp
@@ -0,0 +1,37 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008 IMPOMEZIA <schat@impomezia.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtGui>
+
+#include "intropage.h"
+
+IntroPage::IntroPage(QWidget *parent)
+  : QWizardPage(parent)
+{
+  setTitle(tr("Вас приветствует мастер создания дистрибутива для Вашей сети"));
+  setPixmap(QWizard::WatermarkPixmap, QPixmap(":/images/watermark.png"));
+
+  QLabel *introLabel = new QLabel(tr(
+      "<p>Нажмите кнопку <b>&quot;Далее&quot;</b> для продолжения.</p>"
+      ).arg(QApplication::applicationVersion()), this);
+  introLabel->setWordWrap(true);
+
+  QVBoxLayout *mainLay = new QVBoxLayout(this);
+  mainLay->addWidget(introLabel);
+  mainLay->addStretch();
+}
