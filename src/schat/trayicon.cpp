befd631ceaccf3ce5d86acfaae5629500e38f4b6
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-12-26 19:50:09 +0000

    [+] Добавлена поддержка Нового Года.

--- src/schat/trayicon.cpp
+++ src/schat/trayicon.cpp
@@ -9,11 +9,11 @@
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <QtGui>
@@ -27,7 +27,10 @@
 TrayIcon::TrayIcon(QObject *parent)
   : QSystemTrayIcon(parent)
 {
-  m_icon = QIcon(":/images/logo16.png");
+  if (Settings::isNewYear())
+    m_icon = QIcon(":/images/logo16-ny.png");
+  else
+    m_icon = QIcon(":/images/logo16.png");
   setIcon(m_icon);
 
   init();
