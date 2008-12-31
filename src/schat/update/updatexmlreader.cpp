3a22b2db241c52c565b671c1b7cfeb40fe96f0d5
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2008-12-31 10:44:28 +0000

    [*] [schat] Маленькие оптимизации.

--- src/schat/update/updatexmlreader.cpp
+++ src/schat/update/updatexmlreader.cpp
@@ -9,11 +9,11 @@
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <QtCore>
@@ -38,7 +38,11 @@ bool UpdateXmlReader::isValid() const
   if (error())
     return false;
 
-  if (m_version.isEmpty() || m_version.isEmpty())
+  #ifndef SCHAT_NO_UPDATE
+    if (m_version.isEmpty() || m_files.isEmpty())
+  #else
+    if (m_version.isEmpty())
+  #endif
     return false;
 
   return true;
