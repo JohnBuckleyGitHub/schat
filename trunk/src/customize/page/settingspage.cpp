59a26231026c48074505f835112bc73415feb12c
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-01-02 13:48:11 +0000

    [*] Настройки по умолчанию теперь соответствуют публичной версии.

--- trunk/src/customize/page/settingspage.cpp
+++ trunk/src/customize/page/settingspage.cpp
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008 - 2009 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -45,7 +45,7 @@ SettingsPage::SettingsPage(QWidget *parent)
   m_overrideMirror = new QCheckBox(tr("&Зеркало обновлений"), this);
   m_overrideMirror->setToolTip(tr("Установить альтернативный адрес источника обновлений"));
   m_overrideMirror->setChecked(m_settings->getBool("OverrideMirror"));
-  m_mirror = new QLineEdit(m_settings->getString("MirrorUrl"), this); /// \todo Адрес должен быть в интернете.
+  m_mirror = new QLineEdit(m_settings->getString("MirrorUrl"), this);
 
   QGroupBox *group = new QGroupBox(tr("Основные настройки"), this);
   QGridLayout *groupLay = new QGridLayout(group);
