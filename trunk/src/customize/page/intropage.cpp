0bb7fa7f37f09fd8ee8cabff365cdadffdbd0fce
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-01-03 15:16:51 +0000

    [*] [customize] Реализована функциональность `IntroPage`.

--- trunk/src/customize/page/intropage.cpp
+++ trunk/src/customize/page/intropage.cpp
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008 - 2009 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -26,13 +26,17 @@
 IntroPage::IntroPage(QWidget *parent)
   : QWizardPage(parent)
 {
-  setTitle(tr("Вас приветствует мастер создания дистрибутива для Вашей сети"));
+  setTitle(tr("Вас приветствует мастер создания дистрибутива для вашей сети"));
   setPixmap(QWizard::WatermarkPixmap, QPixmap(":/images/watermark.png"));
 
   QLabel *introLabel = new QLabel(tr(
+      "<p>Этот мастер поможет создать дистрибутив адаптированный для вашей сети,"
+      " и даже создать локальное зеркало обновлений.</p>"
+      "Обратитесь к <a href='http://simple.impomezia.com/Customize' style='text-decoration:none; color:#1a4d82;'>документации</a> за подробностями."
       "<p>Нажмите кнопку <b>&quot;Далее&quot;</b> для продолжения.</p>"
       ), this);
   introLabel->setWordWrap(true);
+  introLabel->setOpenExternalLinks(true);
 
   QVBoxLayout *mainLay = new QVBoxLayout(this);
   mainLay->addWidget(introLabel);
