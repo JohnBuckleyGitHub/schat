aa8ddd19cbd97ca92d3d3e3f93162985bc73e5f9
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-01-04 00:48:14 +0000

    [*] [customize] Исправлены множественные ошибки при обновлении mirror.xml.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -19,14 +19,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION      "0.4.0.855 TRUNK"
-#define SCHAT_VERSION_RC   0,4,0,855
+#define SCHAT_VERSION      "0.4.0.856 TRUNK"
+#define SCHAT_VERSION_RC   0,4,0,856
 #define SCHAT_NAME         "IMPOMEZIA Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "impomezia.com"
 #define SCHAT_COPYRIGHT    "Copyright © 2008 - 2009 IMPOMEZIA"
 
 static const int UpdateLevelQt   = 2009010201;
-static const int UpdateLevelCore = 2009010300;
+static const int UpdateLevelCore = 2009010400;
 
 #endif /*VERSION_H_*/
