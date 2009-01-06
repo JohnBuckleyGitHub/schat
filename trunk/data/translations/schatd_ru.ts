882a1fec742ddf7c84122b5ac0c853c9b09c92bd
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-01-06 10:36:44 +0000

    [*] Улучшена обработка команды /motd, теперь количество пользователей/серверов в ${USERS} и ${SERVERS} выделяется полужирным.

--- trunk/data/translations/schatd_ru.ts
+++ trunk/data/translations/schatd_ru.ts
@@ -5,20 +5,20 @@
     <name>Daemon</name>
     <message numerus="yes">
         <location filename="../../src/schatd/daemon.cpp" line="597"/>
-        <source>%n &#xd0;&#xbf;&#xd0;&#xbe;&#xd0;&#xbb;&#xd1;&#x8c;&#xd0;&#xb7;&#xd0;&#xbe;&#xd0;&#xb2;&#xd0;&#xb0;&#xd1;&#x82;&#xd0;&#xb5;&#xd0;&#xbb;&#xd1;&#x8c;</source>
+        <source>&lt;b&gt;%n&lt;/b&gt; &#xd0;&#xbf;&#xd0;&#xbe;&#xd0;&#xbb;&#xd1;&#x8c;&#xd0;&#xb7;&#xd0;&#xbe;&#xd0;&#xb2;&#xd0;&#xb0;&#xd1;&#x82;&#xd0;&#xb5;&#xd0;&#xbb;&#xd1;&#x8c;</source>
         <translation>
-            <numerusform>%n пользователь</numerusform>
-            <numerusform>%n пользователя</numerusform>
-            <numerusform>%n пользователей</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; пользователь</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; пользователя</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; пользователей</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/schatd/daemon.cpp" line="599"/>
-        <source>%n &#xd1;&#x81;&#xd0;&#xb5;&#xd1;&#x80;&#xd0;&#xb2;&#xd0;&#xb5;&#xd1;&#x80;</source>
+        <source>&lt;b&gt;%n&lt;/b&gt; &#xd1;&#x81;&#xd0;&#xb5;&#xd1;&#x80;&#xd0;&#xb2;&#xd0;&#xb5;&#xd1;&#x80;</source>
         <translation>
-            <numerusform>%n сервер</numerusform>
-            <numerusform>%n сервера</numerusform>
-            <numerusform>%n серверов</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; сервер</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; сервера</numerusform>
+            <numerusform>&lt;b&gt;%n&lt;/b&gt; серверов</numerusform>
         </translation>
     </message>
 </context>
