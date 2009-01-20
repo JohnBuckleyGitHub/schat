fa9ba9f8534ab969697cdf6a6cb96cd3d2831ab1
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-01-20 20:09:55 +0000

    [*] Beta Release: 0.4.1.886.

--- src/common/version.h
+++ src/common/version.h
@@ -19,14 +19,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION      "0.4.1.883 TRUNK"
-#define SCHAT_VERSION_RC   0,4,1,883
+#define SCHAT_VERSION      "0.4.1.886 Beta"
+#define SCHAT_VERSION_RC   0,4,1,886
 #define SCHAT_NAME         "IMPOMEZIA Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "impomezia.com"
 #define SCHAT_COPYRIGHT    "Copyright © 2008 - 2009 IMPOMEZIA"
 
 static const int UpdateLevelQt   = 2009010201;
-static const int UpdateLevelCore = 2009012000;
+static const int UpdateLevelCore = 2009012001;
 
 #endif /*VERSION_H_*/
