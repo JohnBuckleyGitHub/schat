3f0cd51fd6c8466873701ac97a505769bf38d32f
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-02-11 18:06:02 +0000

    [*] [schatd] Исправлена критическая ошибка при синхронизации списка пользователей при многосерверной конфигурации.

--- src/common/version.h
+++ src/common/version.h
@@ -19,14 +19,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION      "0.5.0.958 TRUNK"
-#define SCHAT_VERSION_RC   0,5,0,958
+#define SCHAT_VERSION      "0.5.0.967 TRUNK"
+#define SCHAT_VERSION_RC   0,5,0,967
 #define SCHAT_NAME         "IMPOMEZIA Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "impomezia.com"
 #define SCHAT_COPYRIGHT    "Copyright © 2008 - 2009 IMPOMEZIA"
 
 static const int UpdateLevelQt   = 2009013101;
-static const int UpdateLevelCore = 2009020900;
+static const int UpdateLevelCore = 2009021100;
 
 #endif /*VERSION_H_*/
