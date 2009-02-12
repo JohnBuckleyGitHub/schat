f1acff9dfd250997bea3196a96c434efe2ca388a
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-02-12 19:41:15 +0000

    [*] Промежуточный релиз.

--- src/schat/schatwindow.cpp
+++ src/schat/schatwindow.cpp
@@ -197,6 +197,7 @@ QString SChatWindowPrivate::channel()
 void SChatWindowPrivate::cmdHelp(AbstractTab *tab, const QString &cmd)
 {
   if (cmds.isEmpty()) {
+    cmds.insert("away",   QObject::tr("<b>/away</b><span class='info'> — включает/выключает статус <b>Отсутствую</b>.</span>"));
     cmds.insert("bye",    QObject::tr("<b>/bye [текст сообщения]</b><span class='info'> — отключится от сервера/сети, опционально можно указать альтернативное сообщение о выходе.</span>"));
     cmds.insert("clear",  QObject::tr("<b>/clear</b><span class='info'> — очистка окна чата.</span>"));
     cmds.insert("exit",   QObject::tr("<b>/exit</b><span class='info'> — выход из чата.</span>"));
