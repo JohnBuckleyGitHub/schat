a14d9d26190889ab381bb667da7f392661d47844
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-02-14 23:48:19 +0000

    [*] [schat 0.4] Небольшие интерфейсные улучшения, позаимствованные из версии 0.5.

--- src/schat/chatbrowser.cpp
+++ src/schat/chatbrowser.cpp
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008 - 2009 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2009 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -37,6 +37,7 @@ ChatBrowser::ChatBrowser(QWidget *parent)
   : QTextBrowser(parent), m_log(false)
 {
   setFocusPolicy(Qt::NoFocus);
+  setFrameShape(QFrame::NoFrame);
   setOpenLinks(false);
   m_settings = settings;
 
