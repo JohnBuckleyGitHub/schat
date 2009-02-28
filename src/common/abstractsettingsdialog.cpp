af31981c1b45c371e24488697651100e673ee768
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-02-28 01:38:16 +0000

    [*] Исправлено появление полосы прокрутки в диалоге настройки в Windows Vista.

--- src/common/abstractsettingsdialog.cpp
+++ src/common/abstractsettingsdialog.cpp
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2009 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -9,11 +9,11 @@
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <QtGui>
@@ -78,7 +78,7 @@ void AbstractSettingsDialog::setPage(int page)
 void AbstractSettingsDialog::showEvent(QShowEvent *event)
 {
   if (m_badSize) {
-    m_contentsWidget->setMinimumSize(m_contentsWidget->size());
+    m_contentsWidget->setMinimumSize(m_contentsWidget->width() + 8, m_contentsWidget->height());
     resize(100, 100);
     m_contentsWidget->setCurrentRow(0);
     m_badSize = false;
