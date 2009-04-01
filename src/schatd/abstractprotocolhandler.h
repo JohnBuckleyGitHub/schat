a55668f8640735d811a326893884649afae09358
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-04-01 20:02:10 +0000

    [+] [schatd] Добавлены базовые каркасы классов AbstractProtocolHandler и SimpleClientHandler.

---
+++ src/schatd/abstractprotocolhandler.h
@@ -0,0 +1,44 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008-2009 IMPOMEZIA <schat@impomezia.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ABSTRACTPROTOCOLHANDLER_H_
+#define ABSTRACTPROTOCOLHANDLER_H_
+
+/*!
+ * \brief Базовый класс для обработки специфичный протоколов.
+ */
+class AbstractProtocolHandler {
+
+public:
+  /// Тип протокола.
+  enum Type {
+    Basic,              ///< Базовый протокол.
+    SimpleClientServer, ///< Протокол обмена между клиентом и сервером.
+    SimpleServerServer  ///< Протокол обмена между серверами.
+  };
+
+  AbstractProtocolHandler(Type type = Basic);
+  virtual ~AbstractProtocolHandler();
+  inline Type type() const { return m_type; }
+
+private:
+  const Type m_type;
+};
+
+
+#endif /* ABSTRACTPROTOCOLHANDLER_H_ */
