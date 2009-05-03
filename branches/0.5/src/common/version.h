89bfd38e614cccff5b63ada38fd46c30d651771a
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-05-03 14:21:38 +0000

    * RELEASE 0.5.3.

--- branches/0.5/src/common/version.h
+++ branches/0.5/src/common/version.h
@@ -19,14 +19,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION      "0.5.3.1103 DEV"
-#define SCHAT_VERSION_RC   0,5,3,1103
+#define SCHAT_VERSION      "0.5.3.1106 Beta"
+#define SCHAT_VERSION_RC   0,5,3,1106
 #define SCHAT_NAME         "IMPOMEZIA Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "impomezia.com"
 #define SCHAT_COPYRIGHT    "Copyright © 2008 - 2009 IMPOMEZIA"
 
-static const int UpdateLevelQt   = 2009042400;
-static const int UpdateLevelCore = 2009050200;
+static const int UpdateLevelQt   = 2009050400;
+static const int UpdateLevelCore = 2009050400;
 
 #endif /*VERSION_H_*/
