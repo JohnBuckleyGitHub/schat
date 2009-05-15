d7bae9dcce14a313a49deffa9702edfeb68f8378
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-05-15 16:36:00 +0000

    [*] [schat] Переименование класса Tab в PrivateTab [2/2].

---
+++ src/schat/privatetab.cpp
@@ -0,0 +1,46 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008-2009 IMPOMEZIA <schat@impomezia.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtGui>
+
+#include "privatetab.h"
+#include "settings.h"
+
+/*!
+ * \brief Конструктор класса Tab.
+ */
+PrivateTab::PrivateTab(const QIcon &icon, QWidget *parent)
+  : AbstractTab(icon, parent)
+{
+  m_type = Private;
+
+  QVBoxLayout *mainLay = new QVBoxLayout(this);
+  mainLay->addWidget(m_view);
+  mainLay->setMargin(0);
+
+  m_settings = settings;
+  m_view->log(m_settings->getBool("LogPrivate"));
+  connect(m_settings, SIGNAL(changed(int)), SLOT(notify(int)));
+}
+
+
+void PrivateTab::notify(int code)
+{
+  if (code == Settings::MiscSettingsChanged)
+    m_view->log(m_settings->getBool("LogPrivate"));
+}
