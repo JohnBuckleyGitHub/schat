66281c6ca96bfd67ac12c1533e1dbba6cd480d13
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-05-26 23:03:08 +0000

    [*] [schat] Исправлено, для некоторых элементов в журнале не было стилей.

--- src/common/version.h
+++ src/common/version.h
@@ -19,14 +19,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION      "0.7.0.1144 TRUNK"
-#define SCHAT_VERSION_RC   0,7,0,1144
+#define SCHAT_VERSION      "0.7.0.1149 TRUNK"
+#define SCHAT_VERSION_RC   0,7,0,1149
 #define SCHAT_NAME         "IMPOMEZIA Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "impomezia.com"
 #define SCHAT_COPYRIGHT    "Copyright © 2008-2009 IMPOMEZIA"
 
 static const int UpdateLevelQt   = 2009050400;
-static const int UpdateLevelCore = 2009052600;
+static const int UpdateLevelCore = 2009052700;
 
 #endif /*VERSION_H_*/
