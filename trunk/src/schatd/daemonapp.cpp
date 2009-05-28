ad1412f9cb4670ba88f6daa3f35d73121af8d46b
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-05-28 02:32:53 +0000

    [+] [schatd] Добавлен класс DaemonApp интегрирующий поддержку QtService. [+] Добавлена библиотека QtService.

---
+++ trunk/src/schatd/daemonapp.cpp
@@ -0,0 +1,69 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008-2009 IMPOMEZIA <schat@impomezia.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtCore>
+
+#include "daemon.h"
+#include "daemonapp.h"
+
+/*!
+ * Конструктор класса DaemonApp.
+ */
+DaemonApp::DaemonApp(int argc, char **argv)
+  : QtService<QCoreApplication>(argc, argv, "Simple Chat Daemon")
+{
+  QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+  QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+
+  setServiceDescription("IMPOMEZIA Simple Chat Daemon");
+}
+
+
+/*!
+ * Запуск сервера.
+ */
+void DaemonApp::start()
+{
+  QCoreApplication *app = application();
+  QTranslator *translator = new QTranslator(app);
+  translator->load("schatd_ru", ":/translations");
+  app->installTranslator(translator);
+
+  Daemon *daemon = new Daemon(app);
+
+  if (!daemon->start()) {
+    logMessage("Failed to start Simple Chat Daemon, see logs", QtServiceBase::Error);
+    app->exit(3);
+  }
+
+  QFile pidfile(app->applicationDirPath() + "/schatd.pid");
+  if (pidfile.open(QIODevice::WriteOnly | QIODevice::Text)) {
+    QTextStream out(&pidfile);
+    out << app->applicationPid();
+    pidfile.close();
+  }
+}
+
+
+/*!
+ * Остановка сервиса.
+ */
+void DaemonApp::stop()
+{
+  QFile::remove(application()->applicationDirPath() + "/schatd.pid");
+}
