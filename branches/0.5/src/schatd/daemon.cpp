9c574326bbcbb22be8b5a21a428bbd71d3e8dedc
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-05-30 23:00:45 +0000

    [*] [schatd] Перенос r1156 в стабильную ветку.

--- branches/0.5/src/schatd/daemon.cpp
+++ branches/0.5/src/schatd/daemon.cpp
@@ -100,14 +100,23 @@ bool Daemon::start()
   m_stats = initStats();
 
   #ifndef SCHAT_NO_LOCAL_SERVER
-    if (m_settings->getBool("LocalServer")) {
-      m_localServer = new QLocalServer(this);
-      if (m_localServer->listen(QCryptographicHash::hash(qApp->applicationDirPath().toUtf8(), QCryptographicHash::Md5).toHex())) {
-        connect(m_localServer, SIGNAL(newConnection()), SLOT(incomingLocalConnection()));
-      }
-      else
-        m_localServer->deleteLater();
-    }
+  if (m_settings->getBool("LocalServer")) {
+    QString serverName = QCryptographicHash::hash(QCoreApplication::applicationDirPath().toUtf8(), QCryptographicHash::Md5).toHex();
+    #ifdef Q_OS_UNIX
+     #if QT_VERSION >= 0x040500
+      QLocalServer::removeServer(serverName);
+     #else
+      QString fullServerName = QDir::tempPath() + QLatin1String("/") + serverName;
+      if (QFile::exists(fullServerName))
+        QFile::remove(fullServerName);
+     #endif
+    #endif
+    m_localServer = new QLocalServer(this);
+    if (m_localServer->listen(serverName))
+      connect(m_localServer, SIGNAL(newConnection()), SLOT(incomingLocalConnection()));
+    else
+      m_localServer->deleteLater();
+  }
   #endif /*SCHAT_NO_LOCAL_SERVER*/
 
   QString address = m_settings->getString("ListenAddress");
