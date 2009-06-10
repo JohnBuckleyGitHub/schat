b916fbb3796fd1cc280cffb4c1cbdce0f0997e22
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-06-10 23:31:29 +0000

    * RELEASE 0.5.4. (исправлена ошибка с версией).

--- branches/0.5/misc/tools/tarball.sh
+++ branches/0.5/misc/tools/tarball.sh
@@ -16,7 +16,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-FNAME="schat-0.5.4.1165"
+FNAME="schat-0.5.4.1166"
 svn co http://schat.googlecode.com/svn/branches/0.5/ schat
 rm -fr $FNAME
 cp -fR schat $FNAME
