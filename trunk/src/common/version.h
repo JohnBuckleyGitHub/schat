7e0233dbbde46fd9eadfbe20b850b30faf5ac9d4
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-07-20 20:40:48 +0000

    [*] [schatd] Изменение структуры init.d скриптов Gentoo Linux [2/2].

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -19,14 +19,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION      "0.7.0.1181 TRUNK"
-#define SCHAT_VERSION_RC   0,7,0,1181
+#define SCHAT_VERSION      "0.7.0.1184 TRUNK"
+#define SCHAT_VERSION_RC   0,7,0,1184
 #define SCHAT_NAME         "IMPOMEZIA Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "impomezia.ru"
 #define SCHAT_COPYRIGHT    "Copyright © 2008-2009 IMPOMEZIA"
 
 static const int UpdateLevelQt   = 2009062700;
-static const int UpdateLevelCore = 2009072000;
+static const int UpdateLevelCore = 2009072100;
 
 #endif /*VERSION_H_*/
