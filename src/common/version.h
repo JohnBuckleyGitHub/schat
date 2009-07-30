dd61543cde26717c20d8c3aad112cbffa9c02901
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2009-07-30 20:31:53 +0000

    [*] [schatd] Исправление ошибки в r1188. [-] Удалён AchimNet.xml.

--- src/common/version.h
+++ src/common/version.h
@@ -19,14 +19,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION      "0.7.0.1189 TRUNK"
-#define SCHAT_VERSION_RC   0,7,0,1189
+#define SCHAT_VERSION      "0.7.0.1190 TRUNK"
+#define SCHAT_VERSION_RC   0,7,0,1190
 #define SCHAT_NAME         "IMPOMEZIA Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "impomezia.ru"
 #define SCHAT_COPYRIGHT    "Copyright © 2008-2009 IMPOMEZIA"
 
 static const int UpdateLevelQt   = 2009062700;
-static const int UpdateLevelCore = 2009072900;
+static const int UpdateLevelCore = 2009073000;
 
 #endif /*VERSION_H_*/
