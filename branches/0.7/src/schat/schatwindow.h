e6c0bb49b21691ce83ac156c1cecfd9e054756c7
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2010-01-09 00:49:39 +0000

    * Перенос исправлений r1243 и r1244 в стабильную ветку. * Тестовый релиз.

--- branches/0.7/src/schat/schatwindow.h
+++ branches/0.7/src/schat/schatwindow.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2009 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2010 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -43,6 +43,7 @@ protected:
   bool event(QEvent *event);
   void closeEvent(QCloseEvent *event);
   void keyPressEvent(QKeyEvent *event);
+  void showEvent(QShowEvent *event);
 
 private slots:
   void about();
