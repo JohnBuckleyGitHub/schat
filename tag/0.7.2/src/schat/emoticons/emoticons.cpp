b988a6f8b5c13322c4ca6ba5b29e30b031453152
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2010-04-18 19:14:43 +0000

    TAG 0.7.2.

---
+++ tag/0.7.2/src/schat/emoticons/emoticons.cpp
@@ -0,0 +1,194 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008-2009 IMPOMEZIA <schat@impomezia.com>
+ *
+ * Base class Emoticons
+ * Copyright © 2008 by Carlo Segato <brandon.ml@gmail.com>
+ * Copyright © 2008 Montel Laurent <montel@kde.org>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtGui>
+
+#include "emoticons.h"
+#include "emoticonsprovider.h"
+#include "providers/adium/adium_emoticons.h"
+#include "providers/kde/kde_emoticons.h"
+#include "providers/pidgin/pidgin_emoticons.h"
+#include "providers/xmpp/xmpp_emoticons.h"
+#include "settings.h"
+
+class EmoticonsPrivate
+{
+public:
+  EmoticonsPrivate(Emoticons *parent);
+  ~EmoticonsPrivate();
+  QHash<QString, EmoticonsTheme> m_themes;
+  Emoticons *q;
+};
+
+
+EmoticonsPrivate::EmoticonsPrivate(Emoticons *parent)
+  : q(parent)
+{
+}
+
+
+EmoticonsPrivate::~EmoticonsPrivate()
+{
+}
+
+
+/*!
+ * Default constructor.
+ */
+Emoticons::Emoticons(QObject *parent)
+  : QObject(parent), d(new EmoticonsPrivate(this))
+{
+}
+
+
+/*!
+ * Destruct the object.
+ */
+Emoticons::~Emoticons()
+{
+  delete d;
+}
+
+
+/*!
+ * Retrieve the current emoticons theme.
+ *
+ * \return the current EmoticonsTheme.
+ */
+EmoticonsTheme Emoticons::theme()
+{
+  return theme(currentThemeName());
+}
+
+
+/*!
+ * Retrieve the theme with name \p name.
+ *
+ * \param name name of the theme.
+ * \return the EmoticonsTheme \p name.
+ */
+EmoticonsTheme Emoticons::theme(const QString &name)
+{
+  if (d->m_themes.contains(name))
+    return d->m_themes.value(name);
+
+  QString path = QApplication::applicationDirPath() + "/emoticons/" + name + '/';
+  EmoticonsProvider *provider = 0;
+
+  if (QFile::exists(path + "icondef.xml")) {
+    provider = new XmppEmoticons(this);
+    path += "icondef.xml";
+  }
+  else if (QFile::exists(path + "emoticons.xml")) {
+    provider = new KdeEmoticons(this);
+    path += "emoticons.xml";
+  }
+  else if (QFile::exists(path + "theme")) {
+    provider = new PidginEmoticons(this);
+    path += "theme";
+  }
+  else if (QFile::exists(path + "Emoticons.plist")) {
+    provider = new AdiumEmoticons(this);
+    path += "Emoticons.plist";
+  }
+
+  if (provider) {
+    EmoticonsTheme theme(provider);
+    theme.loadTheme(path);
+    d->m_themes.insert(name, theme);
+    return theme;
+  }
+
+  return EmoticonsTheme();
+}
+
+
+bool Emoticons::strictParse()
+{
+  return settings->getBool("EmoticonsRequireSpaces");
+}
+
+
+/*!
+ * Retrieve the current emoticon theme name.
+ */
+QString Emoticons::currentThemeName()
+{
+  return settings->getString("EmoticonTheme");
+}
+
+
+/*!
+ * Returns a list of installed theme.
+ *
+ * \todo Невалидные темы не должны добавляться в список!
+ */
+QStringList Emoticons::themeList()
+{
+  QStringList ls;
+  QStringList themeDirs; /// \todo Добавить другие пути к темам смайликов.
+  themeDirs << (QApplication::applicationDirPath() + "/emoticons/");
+
+  for (int i = 0; i < themeDirs.count(); ++i) {
+    QDir themeQDir(themeDirs.at(i));
+    themeQDir.setFilter(QDir::Dirs | QDir::NoDotAndDotDot);
+    themeQDir.setSorting(QDir::Name);
+    QString dir = themeDirs.at(i);
+    foreach (QString theme, themeQDir.entryList()) {
+      if (QFile::exists(dir + theme + "/icondef.xml") || QFile::exists(dir + theme + "/emoticons.xml") || QFile::exists(dir + theme + "/theme") || QFile::exists(dir + theme + "/Emoticons.plist"))
+        ls << theme;
+    }
+  }
+
+  return ls;
+}
+
+
+/*!
+ * Устанавливает режим парсинга.
+ */
+void Emoticons::setStrictParse(bool strict)
+{
+  settings->setBool("EmoticonsRequireSpaces", strict);
+}
+
+
+/*!
+ * Set \p theme as the current theme.
+ *
+ * \param theme a pointer to a EmoticonsTheme object.
+ */
+void Emoticons::setTheme(const EmoticonsTheme &theme)
+{
+  setTheme(theme.themeName());
+}
+
+
+/*!
+ * Set \p theme as the current theme.
+ *
+ * \param theme the name of a theme.
+ */
+void Emoticons::setTheme(const QString &theme)
+{
+  settings->setString("EmoticonTheme", theme);
+}
---
+++ tag/0.7.2/src/schat/widget/sendwidget.h
@@ -0,0 +1,106 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008-2009 IMPOMEZIA <schat@impomezia.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SENDWIDGET_H_
+#define SENDWIDGET_H_
+
+#include <QPointer>
+#include <QTextCharFormat>
+#include <QWidget>
+
+#include "widget/inputwidget.h"
+
+class ColorButton;
+class EmoticonSelector;
+class InputWidget;
+class QAction;
+class QToolBar;
+class QToolButton;
+class SoundAction;
+
+/*!
+ * \brief Виджет полностью берущий на себя ввода текста.
+ */
+class SendWidget : public QWidget
+{
+  Q_OBJECT
+
+public:
+  SendWidget(QWidget *parent = 0);
+  inline QAction* aboutAction() const { return m_aboutAction; }
+  inline QAction* quitAction() const  { return m_quitAction; }
+  inline void clear()                 { m_input->clearMsg(); }
+  QPair<SoundAction*, bool> soundAction() const;
+  QToolButton* settingsButton() const;
+
+signals:
+  void about();
+  void closeChat();
+  void needCopy();
+  void sendMsg(const QString &message);
+  void showSettingsPage(int page);
+  void statusShortcut(int key);
+
+public slots:
+  void copy()                          { m_input->copy(); }
+  void insertHtml(const QString &text) { m_input->insertHtml(text); }
+
+private slots:
+  void cursorPositionChanged();
+  void log();
+  void setBold(bool b);
+  void setItalic(bool b);
+  void setSettings();
+  void setStrike(bool b);
+  void settingsPage();
+  void setUnderline(bool b);
+  void textColor(const QColor &color);
+
+private:
+  bool eventFilter(QObject *object, QEvent *event);
+  QAction* createAction(const QString &name, QAction *before = 0);
+  QAction* createSettingsPage(QMenu *menu, const QIcon &icon, const QString &text, int page);
+  QMenu* availableActions();
+  QStringList toolBarLayout() const;
+  void availableAction(QMenu *menu, const QString &name, const QIcon &icon, const QString &text) const;
+  void buildToolBar(const QStringList &actions, bool forceSend = true);
+  void clearToolBar();
+  void createPermanentButtons();
+  void initToolBar();
+  void mergeFormat(const QTextCharFormat &format);
+  void saveToolBarLayout();
+
+  const bool m_bigSendButton;        ///< Опция настроек "BigSendButton".
+  InputWidget* const m_input;        ///< Виджет ввода текста.
+  QAction *m_aboutAction;            ///< "О Simple Chat...".
+  QAction *m_quitAction;             ///< "Выход".
+  QPointer<ColorButton> m_color;     ///< Кнопка для цветового веделения.
+  QPointer<QAction> m_bold;          ///< "Полужирный".
+  QPointer<QAction> m_italic;        ///< "Курсив".
+  QPointer<QAction> m_strike;        ///< "Зачёркнутый".
+  QPointer<QAction> m_underline;     ///< "Подчёркнутый".
+  QPointer<QToolButton> m_emoticons; ///< Кнопка для выбора смайлов.
+  QPointer<QToolButton> m_send;      ///< Кнопка отправки.
+  QStringList m_availableActions;    ///< Список кнопок которые можно добавить на панель инструментов.
+  QStringList m_permanentButtons;    ///< Список не удаляемых кнопок.
+  QToolBar *m_toolBar;               ///< Панель инструментов.
+  QToolButton *m_settingsButton;     ///< Кнопка с меню настроек.
+  SoundAction *m_soundAction;        ///< Действие для включения/выключения звука.
+};
+
+#endif /*SENDWIDGET_H_*/
