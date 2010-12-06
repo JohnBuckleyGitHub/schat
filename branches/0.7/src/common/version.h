23d2c69183f70a5ba15d5f3c4cd90ebf78c82ef5
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2010-12-06 05:10:33 +0000

    + В сеть Simple Network добавлен сервер расположенный в США.

--- branches/0.7/src/common/version.h
+++ branches/0.7/src/common/version.h
@@ -19,14 +19,14 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define SCHAT_VERSION      "0.7.4.1350 Beta"
-#define SCHAT_VERSION_RC   0,7,4,1350
+#define SCHAT_VERSION      "0.7.5.1353 Beta"
+#define SCHAT_VERSION_RC   0,7,5,1353
 #define SCHAT_NAME         "IMPOMEZIA Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "impomezia.ru"
 #define SCHAT_COPYRIGHT    "Copyright © 2008-2010 IMPOMEZIA"
 
 static const int UpdateLevelQt   = 2010111600;
-static const int UpdateLevelCore = 2010111600;
+static const int UpdateLevelCore = 2010120600;
 
 #endif /*VERSION_H_*/
