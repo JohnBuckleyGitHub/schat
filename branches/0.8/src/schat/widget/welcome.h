8bc035a592d90f7b01b2d0881a054cdb389319a4
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2011-01-03 16:21:51 +0000

    + [schat] В диалог приветствия добавлена возможность выбора языка.

--- branches/0.8/src/schat/widget/welcome.h
+++ branches/0.8/src/schat/widget/welcome.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2010 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2011 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -21,6 +21,7 @@
 
 #include "translatewidget.h"
 
+class LanguageBox;
 class NetworkWidget;
 class ProfileWidget;
 class QCheckBox;
@@ -39,12 +40,14 @@ protected:
   void keyPressEvent(QKeyEvent *event);
 
 private slots:
+  void languageChanged(const QString &text);
   void link();
   void validNick(bool valid);
 
 private:
   void retranslateUi();
 
+  LanguageBox *m_language;
   NetworkWidget *m_network;
   ProfileWidget *m_profile;
   QCheckBox *m_ask;
