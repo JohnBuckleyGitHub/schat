50d5c9c91d097bad380d727a93c0227440d62364
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2011-07-15 07:19:25 +0000

    * [schat2] Изменена страница приветствия.

--- src/schat2/ui/tabs/WelcomeTab.cpp
+++ src/schat2/ui/tabs/WelcomeTab.cpp
@@ -16,36 +16,74 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QFormLayout>
+#include <QGridLayout>
+#include <QLabel>
 #include <QLineEdit>
+#include <QPushButton>
 
 #include "ChatCore.h"
 #include "net/SimpleClient.h"
+#include "ui/fields/GenderField.h"
 #include "ui/fields/LanguageField.h"
 #include "ui/fields/NickEdit.h"
 #include "ui/NetworkWidget.h"
 #include "ui/tabs/WelcomeTab.h"
 #include "User.h"
 
-WelcomeTab::WelcomeTab(SimpleClient *client, TabWidget *parent)
+WelcomeTab::WelcomeTab(TabWidget *parent)
   : AbstractTab(QByteArray(), WelcomeType, parent)
-  , m_client(client)
+  , m_client(ChatCore::i()->client())
 {
+  m_networkLabel = new QLabel(this);
   m_networks = new NetworkWidget(this);
-  m_nickEdit = new NickEdit(this);
-  m_languageBox = new LanguageField(this);
+  m_connectButton = new QPushButton(SCHAT_ICON(ConnectIcon), "", this);
+  QGridLayout *networkLay = new QGridLayout;
+  networkLay->addWidget(m_networks, 0, 0, 1, 2);
+  networkLay->addWidget(m_connectButton, 1, 1);
+  networkLay->setContentsMargins(20, 0, 3, 6);
+  networkLay->setColumnStretch(0, 1);
 
-  QFormLayout *mainLay = new QFormLayout(this);
-  mainLay->addRow(tr("Network:"), m_networks);
-  mainLay->addRow(tr("Nick:"), m_nickEdit);
-  mainLay->addRow(tr("Language:"), m_languageBox);
+  m_languageLabel = new QLabel(this);
+  m_languageBox = new LanguageField(this);
+  QGridLayout *languageLay = new QGridLayout;
+  languageLay->addWidget(m_languageBox);
+  languageLay->setContentsMargins(20, 0, 3, 6);
+
+  m_profileLabel = new QLabel(this);
+  m_nickLabel = new QLabel(this);
+  m_nickEdit = new NickEdit(this);
+  m_genderLabel = new QLabel(this);
+  m_genderField = new GenderField(this);
+  QGridLayout *profileLay = new QGridLayout;
+  profileLay->addWidget(m_nickLabel, 0, 0);
+  profileLay->addWidget(m_nickEdit, 0, 1);
+  profileLay->addWidget(m_genderLabel, 1, 0);
+  profileLay->addWidget(m_genderField, 1, 1);
+  profileLay->setContentsMargins(20, 0, 3, 6);
+
+  QGridLayout *mainLay = new QGridLayout(this);
+  mainLay->addWidget(m_networkLabel, 0, 0);
+  mainLay->addWidget(m_languageLabel, 0, 1);
+  mainLay->addLayout(networkLay, 1, 0);
+  mainLay->addLayout(languageLay, 1, 1, Qt::AlignTop);
+  mainLay->addWidget(m_profileLabel, 2, 0);
+  mainLay->addLayout(profileLay, 3, 0);
+  mainLay->addItem(new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding), 4, 0);
 
   setIcon(SCHAT_ICON(SmallLogoIcon));
-  setText(tr("Welcome"));
+  retranslateUi();
+
+  connect(m_connectButton, SIGNAL(clicked()), m_networks, SLOT(open()));
 }
 
 
 void WelcomeTab::retranslateUi()
 {
   setText(tr("Welcome"));
+  m_networkLabel->setText(QLatin1String("<b>") + tr("Network") + QLatin1String("</b>"));
+  m_connectButton->setText(tr("Connect"));
+  m_languageLabel->setText(QLatin1String("<b>") + tr("Language") + QLatin1String("</b>"));
+  m_profileLabel->setText(QLatin1String("<b>") + tr("Profile") + QLatin1String("</b>"));
+  m_nickLabel->setText(tr("Nick:"));
+  m_genderLabel->setText(tr("Gender:"));
 }
