893f348ce7adba14a8bf39f80e539e0649170126
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2011-08-25 19:04:56 +0000

    + [schat] Добавлен украинский перевод интерфейса.

---
+++ branches/0.8/data/translations/schatd-ui_uk.ts
@@ -0,0 +1,331 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>AbstractSettingsDialog</name>
+    <message>
+        <location filename="../../src/common/abstractsettingsdialog.cpp" line="32"/>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <location filename="../../src/common/abstractsettingsdialog.cpp" line="33"/>
+        <source>Cancel</source>
+        <translation>Відміна</translation>
+    </message>
+    <message>
+        <location filename="../../src/common/abstractsettingsdialog.cpp" line="35"/>
+        <source>Restore default settings</source>
+        <translation>Відновити налаштування за замовчуванням</translation>
+    </message>
+    <message>
+        <location filename="../../src/common/abstractsettingsdialog.cpp" line="67"/>
+        <source>Settings</source>
+        <translation>Налаштування</translation>
+    </message>
+</context>
+<context>
+    <name>DaemonCommonSettings</name>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="64"/>
+        <source>The address for the server connection</source>
+        <translation>Адреса, на якій сервер будет очікувати підключення</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="71"/>
+        <source>Port for the server connection</source>
+        <translation>Порт, на якій сервер будет очікувати підключення</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="73"/>
+        <source>&amp;Address:</source>
+        <translation>&amp;Адреса:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="76"/>
+        <source>&amp;Port:</source>
+        <translation>&amp;Порт:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="79"/>
+        <source>Server interface</source>
+        <translation>Інтерфейс сервера</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="92"/>
+        <source>Level of verbose logging
+-1 logging disabled</source>
+        <translation>Рівень деталізації журналу
+-1 журналювання вимкнено</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="94"/>
+        <source>Logging level:</source>
+        <translation>Рівень журналювання:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="97"/>
+        <source>Log the main channel</source>
+        <translation>Вести журнал головного каналу</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="100"/>
+        <source>Logging</source>
+        <translation>Журналювання</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="117"/>
+        <source>Users limit:</source>
+        <translation>Ліміт користувачів:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="119"/>
+        <source>Connection limit from the same address:</source>
+        <translation>Ліміт підключень з однієї адреси:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="122"/>
+        <source>Limits</source>
+        <translation>Обмеження</translation>
+    </message>
+</context>
+<context>
+    <name>DaemonNetSettings</name>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="204"/>
+        <source>Enable the network support</source>
+        <translation>Дозволити підтримку мережі</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="208"/>
+        <source>Root server</source>
+        <translation>Кореневий сервер</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="214"/>
+        <source>Network name</source>
+        <translation>Назва мережі</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="216"/>
+        <source>&amp;Name:</source>
+        <translation>&amp;Назва:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="223"/>
+        <source>&amp;Key:</source>
+        <translation>&amp;Ключ:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="228"/>
+        <source>Root server:</source>
+        <translation>Кореневий сервер:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="231"/>
+        <source>Network</source>
+        <translation>Мережі</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="247"/>
+        <source>Name:</source>
+        <translation>Ім&apos;я:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="258"/>
+        <source>Unique number:</source>
+        <translation>Унікальний номер:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="264"/>
+        <source>Max servers:</source>
+        <translation>Максимум серверів:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="267"/>
+        <source>Server</source>
+        <translation>Сервер</translation>
+    </message>
+</context>
+<context>
+    <name>DaemonServiceSettings</name>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="462"/>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="579"/>
+        <source>not installed</source>
+        <translation>не встановлено</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="464"/>
+        <source>Information</source>
+        <translation>Інформація</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="466"/>
+        <source>Windows service:</source>
+        <translation>Windows сервіс:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="473"/>
+        <source>Install/Remove</source>
+        <translation>Встановлення/Видалення</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="475"/>
+        <source>Service name:</source>
+        <translation>Ім&apos;я сервіса:</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="560"/>
+        <source>Install</source>
+        <translation>Встановити</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="561"/>
+        <source>Install server as service</source>
+        <translation>Встановити сервер как сервіс</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="564"/>
+        <source>Remove</source>
+        <translation>Видалити</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="565"/>
+        <source>Remove service of chat server</source>
+        <translation>Видалити сервіс сервера чата</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="576"/>
+        <source>installed</source>
+        <translation>встановлено</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="582"/>
+        <source>error during installation</source>
+        <translation>помилка при встановленні</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="585"/>
+        <source>error when removing</source>
+        <translation>помилка при видаленні</translation>
+    </message>
+</context>
+<context>
+    <name>DaemonSettingsDialog</name>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="39"/>
+        <source>General</source>
+        <translation>Загальні</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="40"/>
+        <source>Network</source>
+        <translation>Мережа</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonsettingsdialog.cpp" line="42"/>
+        <source>Service</source>
+        <translation>Сервіс</translation>
+    </message>
+</context>
+<context>
+    <name>DaemonUi</name>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="425"/>
+        <source>Control</source>
+        <translation>Управління</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="426"/>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="424"/>
+        <source>Server control</source>
+        <translation>Управління сервером</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="428"/>
+        <source>Quit and stop server</source>
+        <translation>Вихід із зупинкою сервера</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="429"/>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="437"/>
+        <source>Quit</source>
+        <translation>Вихід</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="430"/>
+        <source>Restart</source>
+        <translation>Перезапуск</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="431"/>
+        <source>Start</source>
+        <translation>Запуск</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="432"/>
+        <source>Stop</source>
+        <translation>Зупинка</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="433"/>
+        <source>Settings...</source>
+        <translation>Налаштування...</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="435"/>
+        <source>Hide</source>
+        <translation>Приховати</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="436"/>
+        <source>Hide the program window</source>
+        <translation>Приховати вікно програми</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="483"/>
+        <source>Initialization...</source>
+        <translation>Ініціалізація...</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="489"/>
+        <source>Error</source>
+        <translation>Помилка</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="495"/>
+        <source>Starting...</source>
+        <translation>Запуск...</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="501"/>
+        <source>Started successfully</source>
+        <translation>Успішно запущено</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="507"/>
+        <source>Stopped</source>
+        <translation>Зупинено</translation>
+    </message>
+    <message>
+        <location filename="../../src/schatd-ui/daemonui.cpp" line="513"/>
+        <source>Restart...</source>
+        <translation>Перезапуск...</translation>
+    </message>
+</context>
+<context>
+    <name>Translation</name>
+    <message>
+        <location filename="../../src/common/translation.cpp" line="35"/>
+        <location filename="../../src/common/translation.cpp" line="118"/>
+        <source>English</source>
+        <translation>Українська</translation>
+    </message>
+</context>
+</TS>
