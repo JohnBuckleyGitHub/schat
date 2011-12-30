f14f8d44e75d06cfb227a9dd58256b9797336383
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2011-12-30 21:02:33 +0000

    * Изменён способ передачи заголовка фида

--- src/common/common.pri
+++ src/common/common.pri
@@ -1,6 +1,6 @@
 # $Id$
 # IMPOMEZIA Simple Chat
-# Copyright (c) 2008-2011 IMPOMEZIA <schat@impomezia.com>
+# Copyright (c) 2008-2012 IMPOMEZIA <schat@impomezia.com>
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ INCLUDEPATH += \
     ../common \
     ../3rdparty \
 
-!win32:VERSION = 1.99.2
+!win32:VERSION = 1.99.3
 
 CONFIG(debug, debug|release) { 
   RCC_DIR = ../../tmp/$${TEMPLATE}/$${TARGET}/debug/rcc
--- src/common/feeds/FeedHeader.h
+++ src/common/feeds/FeedHeader.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2011 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -33,24 +33,23 @@ public:
   FeedHeader();
   bool isValid() const;
   inline Acl& acl()                        { return m_acl; }
-  inline Channel *channel() const          { return m_channel; }
   inline const Acl& acl() const            { return m_acl; }
   inline const QString& name() const       { return m_name; }
-  inline qint64 date() const               { return m_date; }
+  inline const QVariantMap& data() const   { return m_data; }
+  inline QVariantMap& data()               { return m_data; }
 
   QVariantMap get(Channel *channel);
-  QVariantMap save() const;
+  QVariantMap save();
 
   inline void setChannel(Channel *channel) { m_channel = channel; }
-  inline void setDate(qint64 date)         { m_date = date; }
   inline void setName(const QString &name) { m_name = name; }
   void setData(const QVariantMap &data);
 
 private:
   Acl m_acl;          ///< Права доступа к фиду.
   Channel *m_channel; ///< Канал фидов.
-  qint64 m_date;      ///< Время последнего обновления фида.
   QString m_name;     ///< Имя фида.
+  QVariantMap m_data; ///< Данные фида.
 };
 
 #endif /* FEEDHEADER_H_ */
