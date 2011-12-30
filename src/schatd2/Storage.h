e51dcd7969894abbfe264244c5e40c0f2391f83a
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2011-12-30 22:56:26 +0000

    * Улучшена инициализация основного канала.

--- src/schatd2/Storage.h
+++ src/schatd2/Storage.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2011 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -63,8 +63,8 @@ public:
   void update(ChatChannel channel);
 
   inline FileLocations *locations() const { return m_locations; }
-  inline ServerData *serverData() { return m_serverData; }
-  inline Settings *settings() { return m_settings; }
+  inline static ServerData *serverData() { return i()->m_serverData; }
+  inline static Settings *settings() { return i()->m_settings; }
   QByteArray cookie() const;
 
 private:
