ed1a8fd396031083e816530d705d142ff87fcd8f
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-01-08 01:50:54 +0000

    * Восстановлена компиляция плагина Slave Node. - Удалён класс NodeKernelApi.

--- src/common/plugins/SlaveNode/SlaveNodePlugin.cpp
+++ src/common/plugins/SlaveNode/SlaveNodePlugin.cpp
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2011 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -21,9 +21,9 @@
 #include "SlaveNode.h"
 #include "SlaveNodePlugin.h"
 
-Core *SlaveNodePlugin::init()
+NodePlugin *SlaveNodePlugin::create()
 {
-  return new SlaveNode(this);
+  return 0;
 }
 
 Q_EXPORT_PLUGIN2(SlaveNode, SlaveNodePlugin);
