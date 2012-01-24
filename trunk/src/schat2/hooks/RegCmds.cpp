6b5a5474221bc33a9f31c3cdacc92bb0d482d678
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-01-24 21:26:55 +0000

    + Добавлена команда /reg.

--- trunk/src/schat2/hooks/RegCmds.cpp
+++ trunk/src/schat2/hooks/RegCmds.cpp
@@ -16,10 +16,13 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "hooks/RegCmds.h"
 #include "client/ChatClient.h"
 #include "client/ClientCmd.h"
 #include "client/ClientMessages.h"
+#include "hooks/RegCmds.h"
+#include "net/SimpleID.h"
+#include "sglobal.h"
+#include "client/ClientFeeds.h"
 
 RegCmds::RegCmds(QObject *parent)
   : Messages(parent)
@@ -28,7 +31,28 @@ RegCmds::RegCmds(QObject *parent)
 }
 
 
-bool RegCmds::command(const QByteArray &dest, const ClientCmd &cmd)
+bool RegCmds::command(const QByteArray & /*dest*/, const ClientCmd &cmd)
 {
+  QString command = cmd.command().toLower();
+  if (command == LS("reg")) {
+    ClientCmd body(cmd.body());
+    if (!body.isValid())
+      return true;
+
+    ChatClient::feeds()->request(ChatClient::id(), LS("query"), LS("account"), reg(body.command(), body.body()));
+    return true;
+  }
+
   return false;
+}
+
+
+QVariantMap RegCmds::reg(const QString &name, const QString &password)
+{
+  QVariantMap out;
+  out[LS("action")] = LS("reg");
+  out[LS("name")]   = name.simplified().toLower().remove(LC(' '));
+  out[LS("pass")]   = SimpleID::encode(SimpleID::password(password));
+
+  return out;
 }
