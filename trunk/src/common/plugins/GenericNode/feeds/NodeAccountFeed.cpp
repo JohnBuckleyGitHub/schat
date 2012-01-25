8f346cc2598fec8d1477c4c2f5b823bef6d78e5e
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-01-25 00:05:28 +0000

    + [GenericNode] Базовая реализация фидом "account" запроса "reg".

--- trunk/src/common/plugins/GenericNode/feeds/NodeAccountFeed.cpp
+++ trunk/src/common/plugins/GenericNode/feeds/NodeAccountFeed.cpp
@@ -16,10 +16,18 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <QDebug>
+
 #include "Account.h"
+#include "Ch.h"
 #include "Channel.h"
+#include "DataBase.h"
 #include "DateTime.h"
+#include "feeds/FeedStorage.h"
 #include "feeds/NodeAccountFeed.h"
+#include "net/packets/Notice.h"
+#include "net/SimpleID.h"
+#include "sglobal.h"
 
 NodeAccountFeed::NodeAccountFeed(const QString &name, const QVariantMap &data)
   : Feed(name, data)
@@ -47,10 +55,69 @@ Feed* NodeAccountFeed::load(const QString &name, const QVariantMap &data)
 }
 
 
+/*!
+ * Обработка запросов.
+ */
+FeedQueryReply NodeAccountFeed::query(const QVariantMap &json, Channel *channel)
+{
+  if (!channel)
+    return FeedQueryReply(Notice::Forbidden);
+
+  if (head().channel()->id() != channel->id())
+    return FeedQueryReply(Notice::Forbidden);
+
+  QString action = json.value(LS("action")).toString();
+  if (action.isEmpty())
+    return FeedQueryReply(Notice::BadRequest);
+
+  if (action == LS("reg"))
+    return reg(json);
+
+  return FeedQueryReply(Notice::NotImplemented);
+}
+
+
 void NodeAccountFeed::setChannel(Channel *channel)
 {
   Feed::setChannel(channel);
 
-  m_data["account"] = channel->account()->name();
-  m_data["groups"]  = channel->account()->groups().toString();
+  m_data[LS("account")] = channel->account()->name();
+  m_data[LS("groups")]  = channel->account()->groups().toString();
+}
+
+
+/*!
+ * Регистрация пользователя.
+ */
+FeedQueryReply NodeAccountFeed::reg(const QVariantMap &json)
+{
+  // Получение и проверка пароля.
+  QByteArray password = SimpleID::decode(json.value(LS("pass")).toString().toLatin1());
+  if (SimpleID::typeOf(password) != SimpleID::PasswordId)
+    return FeedQueryReply(Notice::BadRequest);
+
+  // Получение и проверка имени.
+  QString name = json.value(LS("name")).toString().simplified().toLower().remove(LC(' '));
+  int index = name.indexOf(LC('@'));
+  if (index != -1)
+    name = name.left(index);
+
+  if (name.isEmpty())
+    return FeedQueryReply(Notice::BadRequest);
+
+  // Проверка имени на занятость.
+  qint64 key = DataBase::accountKey(name);
+  if (key != -1)
+    return FeedQueryReply(Notice::ObjectAlreadyExists);
+
+  head().channel()->account()->setName(name);
+  head().channel()->account()->setPassword(password);
+  m_data[LS("account")] = name;
+
+  DataBase::update(Ch::channel(head().channel()->id()));
+  FeedStorage::save(FeedPtr(this));
+
+  FeedQueryReply reply   = FeedQueryReply(Notice::OK);
+  reply.json[LS("name")] = name;
+  return reply;
 }
