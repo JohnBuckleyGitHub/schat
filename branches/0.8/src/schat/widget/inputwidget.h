6f744a31bdf82aced77dffcd00fc8ce745d7099d
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-02-05 12:20:38 +0000

    - [schat_0_8] Удалена возможность переключать статус по Ctrl+цифра.

--- branches/0.8/src/schat/widget/inputwidget.h
+++ branches/0.8/src/schat/widget/inputwidget.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2011 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -36,7 +36,6 @@ public:
 signals:
   void needCopy();
   void sendMsg(const QString &message);
-  void statusShortcut(int key);
 
 public slots:
   void clearMsg();
