a8add87180bc4bdc5d838b18a263e807b626afed
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-02-29 07:13:01 +0000

    * В контекстное меню пользователей добавлен пункт для просмотра профиля.

--- res/translations/schat2-client_en.ts
+++ res/translations/schat2-client_en.ts
@@ -10,32 +10,32 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="167"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="170"/>
         <source>Bad Request</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="170"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="173"/>
         <source>Unauthorized</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="173"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="176"/>
         <source>Forbidden</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="176"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="179"/>
         <source>Not Found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="185"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="188"/>
         <source>Nick Already In Use</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="179"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="182"/>
         <source>Object Already Exists</source>
         <translation type="unfinished"></translation>
     </message>
@@ -45,47 +45,47 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="182"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="185"/>
         <source>Object Not Exists</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="188"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="191"/>
         <source>Channel Offline</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="191"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="194"/>
         <source>Conflict</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="194"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="197"/>
         <source>Internal Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="197"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="200"/>
         <source>Not Implemented</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="200"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="203"/>
         <source>Bad Gateway</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="203"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="206"/>
         <source>Service Unavailable</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="206"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="209"/>
         <source>Gateway Timeout</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/common/net/packets/Notice.cpp" line="209"/>
+        <location filename="../../src/common/net/packets/Notice.cpp" line="212"/>
         <source>Unknown</source>
         <translation type="unfinished"></translation>
     </message>
