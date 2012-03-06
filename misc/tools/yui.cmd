45894c5fc99f7df3e96bc10d47955d6095a3788d
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-03-06 13:44:20 +0000

    + Добавлена базовая поддержка всплывающих подсказок.

--- misc/tools/yui.cmd
+++ misc/tools/yui.cmd
@@ -2,6 +2,8 @@
 java -jar yuicompressor.jar ..\..\res\html\src\ChatView.js -o ..\..\res\html\js\ChatView.min.js
 java -jar yuicompressor.jar ..\..\res\html\src\server.js -o ..\..\res\html\js\server.min.js
 java -jar yuicompressor.jar ..\..\res\html\src\about.js -o ..\..\res\html\js\about.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\easyTooltip.js -o ..\..\res\html\js\easyTooltip.min.js
+
 java -jar yuicompressor.jar ..\..\res\html\src\about.css -o ..\..\res\html\css\about.min.css
 java -jar yuicompressor.jar ..\..\res\html\src\ChatView.css -o ..\..\res\html\css\ChatView.min.css
 java -jar yuicompressor.jar ..\..\res\html\src\server.css -o ..\..\res\html\css\server.min.css
