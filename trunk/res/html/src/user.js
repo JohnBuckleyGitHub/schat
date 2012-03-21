f0b7001fdf312aa51d7bfa226147d7541957fe94
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-03-21 09:15:27 +0000

    + Базовая поддержка отображения текстовых полей профиля.

--- trunk/res/html/src/user.js
+++ trunk/res/html/src/user.js
@@ -15,3 +15,38 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
+var Profile = {
+  read: function(json) {
+    if (!json.hasOwnProperty("head"))
+      return;
+
+    for (var key in json) if (json.hasOwnProperty(key) && key !== "head") {
+      Profile.Field.generic(key, json[key]);
+    }
+  },
+
+  reload: function() {
+    if (Pages.current != 1)
+      return;
+
+    Profile.read(SimpleChat.feed(Settings.id, "profile"));
+  },
+
+  Field: {
+    generic: function(key, json) {
+      if (typeof json !== 'string')
+        return;
+
+      if (json === "")
+        return;
+
+      var out = '<tr><td>' + key + '</td><td>' + json + '</td></tr>';
+      $("#profile-table > tbody").append(out);
+    }
+  }
+};
+
+Pages.onInfo.push(Profile.reload);
+
+ChatView.reload.connect(Profile.reload);
