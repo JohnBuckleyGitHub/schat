aaa4650f57b35d1152caeadf632d56ef1eec2ee5
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-04-06 13:48:19 +0000

    * Флаги перенесены в плагин Profile.

--- misc/tools/yui.cmd
+++ misc/tools/yui.cmd
@@ -10,11 +10,11 @@ java -jar yuicompressor.jar ..\..\res\html\src\ChatView.css -o ..\..\res\html\cs
 java -jar yuicompressor.jar ..\..\res\html\src\server.css -o ..\..\res\html\css\server.min.css
 java -jar yuicompressor.jar ..\..\res\html\src\user.css -o ..\..\res\html\css\user.min.css
 java -jar yuicompressor.jar ..\..\res\html\src\ui.css -o ..\..\res\html\css\ui.min.css
-java -jar yuicompressor.jar ..\..\res\html\src\flags.css -o ..\..\res\html\css\flags.min.css
 
 java -jar yuicompressor.jar ..\..\src\common\plugins\RawFeeds\res\src\RawFeeds.js -o ..\..\src\common\plugins\RawFeeds\res\js\RawFeeds.min.js
 java -jar yuicompressor.jar ..\..\src\common\plugins\RawFeeds\res\src\KelpJSONView.js -o ..\..\src\common\plugins\RawFeeds\res\js\KelpJSONView.min.js
 java -jar yuicompressor.jar ..\..\src\common\plugins\RawFeeds\res\src\RawFeeds.css -o ..\..\src\common\plugins\RawFeeds\res\css\RawFeeds.min.css
 
 java -jar yuicompressor.jar ..\..\src\common\plugins\Profile\res\src\Profile.js -o ..\..\src\common\plugins\Profile\res\js\Profile.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Profile\res\src\flags.css -o ..\..\src\common\plugins\Profile\res\css\flags.min.js
 pause
--- src/common/plugins/Profile/ProfilePlugin.h
+++ src/common/plugins/Profile/ProfilePlugin.h
@@ -33,10 +33,10 @@ public:
     QVariantMap out = CoreApi::header();
     out["Id"]       = "Profile";
     out["Name"]     = "Profile";
-    out["Version"]  = "0.1.1";
+    out["Version"]  = "0.2.0";
     out["Site"]     = "http://wiki.schat.me/Plugin/Profile";
     out["Desc"]     = "Basic Profile";
-    out["Required"] = "1.99.20";
+    out["Required"] = "1.99.22";
 
     return out;
   }
