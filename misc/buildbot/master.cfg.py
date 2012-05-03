6a1fce5f6fc01ead1d50234464f4cff1971c6934
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-05-03 22:55:33 +0000

    * Начало разработки 1.99.27.

--- misc/buildbot/master.cfg.py
+++ misc/buildbot/master.cfg.py
@@ -16,7 +16,7 @@ from buildbot.steps.master import MasterShellCommand
 import schat_passwords
 import os
 
-SCHAT_VERSION        = "1.99.26"
+SCHAT_VERSION        = "1.99.27"
 SCHAT_RELEASE        = False
 SCHAT_UPLOAD_BASE    = "/var/www/download.schat.me/htdocs/schat2/"
 
--- src/common/common.pri
+++ src/common/common.pri
@@ -25,7 +25,7 @@ INCLUDEPATH += \
     ../common \
     ../3rdparty \
 
-!win32:VERSION = 1.99.26
+!win32:VERSION = 1.99.27
 
 CONFIG(debug, debug|release) { 
   RCC_DIR = ../../tmp/$${TEMPLATE}/$${TARGET}/debug/rcc
