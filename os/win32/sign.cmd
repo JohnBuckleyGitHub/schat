f85517b035dfaa5389639ac5820731b1862a4838
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-05-03 18:45:00 +0000

    * [win32] Плагин Idle не подписывался цифровой подписью.

--- os/win32/sign.cmd
+++ os/win32/sign.cmd
@@ -12,3 +12,4 @@ SignTool.exe sign /f %SCHAT_SIGN_FILE% /p %SCHAT_SIGN_PASSWORD% /t http://timest
 SignTool.exe sign /f %SCHAT_SIGN_FILE% /p %SCHAT_SIGN_PASSWORD% /t http://timestamp.comodoca.com/authenticode ../../out/plugins/Profile.dll
 SignTool.exe sign /f %SCHAT_SIGN_FILE% /p %SCHAT_SIGN_PASSWORD% /t http://timestamp.comodoca.com/authenticode ../../out/plugins/GeoIP.dll
 SignTool.exe sign /f %SCHAT_SIGN_FILE% /p %SCHAT_SIGN_PASSWORD% /t http://timestamp.comodoca.com/authenticode ../../out/plugins/Update.dll
+SignTool.exe sign /f %SCHAT_SIGN_FILE% /p %SCHAT_SIGN_PASSWORD% /t http://timestamp.comodoca.com/authenticode ../../out/plugins/Idle.dll
