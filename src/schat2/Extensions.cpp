61115b1b4e9d2ed0b7f910b288750b46c223a37c
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-05-14 05:17:53 +0000

    + Добавлены классы Extensions, Extension, ExtensionInstaller и ExtensionFactory.

---
+++ src/schat2/Extensions.cpp
@@ -0,0 +1,133 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QDebug>
+
+#include <QThreadPool>
+
+#include "Extension.h"
+#include "Extensions.h"
+#include "JSON.h"
+#include "qzip/qzipreader.h"
+#include "sglobal.h"
+
+QString Extensions::m_root;
+
+Extensions::Extensions(QObject *parent)
+  : QObject(parent)
+{
+  m_pool = new QThreadPool(this);
+  m_pool->setMaxThreadCount(1);
+}
+
+
+Extensions::~Extensions()
+{
+  qDeleteAll(m_factory);
+  qDeleteAll(m_extension);
+}
+
+
+/*!
+ * Загрузка расширения.
+ *
+ * \param fileName Имя файла расширения.
+ * \param install  \b true Если необходима автоматическая установка.
+ *
+ * \return 0 в случае ошибки, или созданный объект Extension.
+ */
+Extension* Extensions::load(const QString &fileName, bool install)
+{
+  QVariantMap manifest = Extensions::manifest(fileName);
+  if (manifest.isEmpty())
+    return 0;
+
+  QString id = manifest.value(LS("id")).toString();
+  if (id.isEmpty() || id.contains(LC('/')))
+    return 0;
+
+  QString type = manifest.value(LS("type")).toString();
+  if (type.isEmpty() || type.contains(LC('/')))
+    return 0;
+
+  if (!m_factory.contains(type))
+    return 0;
+
+  Extension *extension = m_factory.value(type)->create(manifest, fileName);
+  if (!extension->isValid()) {
+    delete extension;
+    return 0;
+  }
+
+  QString key = extension->key();
+  if (m_extension.contains(key)) {
+    Extension *e = m_extension.value(key);
+    delete e;
+  }
+
+  m_extension[key] = extension;
+  emit created(key);
+
+  if (install)
+    this->install(extension);
+
+  return extension;
+}
+
+
+void Extensions::addFactory(ExtensionFactory *factory)
+{
+  if (m_factory.contains(factory->type())) {
+    ExtensionFactory *f = m_factory.value(factory->type());
+    delete f;
+  }
+
+  m_factory[factory->type()] = factory;
+}
+
+
+/*!
+ * Низкоуровневая функция для загрузки манифеста расширения.
+ */
+QVariantMap Extensions::manifest(const QString &fileName)
+{
+  if (!QFile::exists(fileName))
+    return QVariantMap();
+
+  QZipReader reader(fileName);
+  if (!reader.isReadable())
+    return QVariantMap();
+
+  return JSON::parse(reader.fileData(LS("manifest.json"))).toMap();
+}
+
+
+void Extensions::install(Extension *extension)
+{
+  if (!extension)
+    return;
+
+  if (extension->isInstalled()) {
+    emit installed(extension->key());
+    return;
+  }
+
+  ExtensionInstaller *installer = extension->installer();
+  connect(installer, SIGNAL(installed(const QString &)), SIGNAL(installed(const QString &)));
+  m_pool->start(installer);
+}
