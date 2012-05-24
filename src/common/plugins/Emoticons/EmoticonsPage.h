a221bb5a40726350de2de58c6c7162de51a51270
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-05-24 18:23:21 +0000

    + [Emoticons] Добавлены классы EmoticonsPage и EmoticonsPageCreator.

---
+++ src/common/plugins/Emoticons/EmoticonsPage.h
@@ -0,0 +1,50 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EMOTICONSPAGE_H_
+#define EMOTICONSPAGE_H_
+
+#include "ui/tabs/SettingsTabHook.h"
+
+class QLabel;
+
+class EmoticonsPage : public SettingsPage
+{
+  Q_OBJECT
+
+public:
+  EmoticonsPage(QWidget *parent = 0);
+
+private:
+  void retranslateUi();
+
+  QLabel *m_label;
+};
+
+
+class EmoticonsPageCreator : public SettingsPageCreator
+{
+public:
+  EmoticonsPageCreator()
+  : SettingsPageCreator(5000)
+  {}
+
+  SettingsPage* page(QWidget *parent = 0);
+};
+
+#endif /* EMOTICONSPAGE_H_ */
