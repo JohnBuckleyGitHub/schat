4e1ac0f7a9d8a80a7dbb53f983f8b2fb12bca48c
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-07-07 23:04:40 +0000

    * Используется <stdint.h> вместо привязки в Qt типам.

--- trunk/src/3rdparty/Tufao/priv/http_parser_qt_helper.h
+++ trunk/src/3rdparty/Tufao/priv/http_parser_qt_helper.h
@@ -5,15 +5,6 @@
 #define HTTP_PARSER_STRICT 0
 #define HTTP_PARSER_DEBUG 0
 
-// types:
-#define int8_t qint8
-#define uint8_t quint8
-#define int16_t qint16
-#define uint16_t quint16
-#define uint32_t quint32
-#define int64_t qint64
-#define uint64_t quint64
-
 // functions:
 #define assert(expr) (expr) ? qt_noop() : qt_assert(#expr, __FILE__, __LINE__)
 #define memset(p, v, s) { \
