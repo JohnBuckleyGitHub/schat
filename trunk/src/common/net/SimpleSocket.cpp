d2cb3d3d8e0e13a8e74a877623318ed106df2cd5
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-07-07 07:10:18 +0000

    * Кандидат в релиз 1.99.35.

--- trunk/src/common/net/SimpleSocket.cpp
+++ trunk/src/common/net/SimpleSocket.cpp
@@ -16,7 +16,7 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-//#define SCHAT_DISABLE_DEBUG
+#define SCHAT_DISABLE_DEBUG
 
 #include <QBasicTimer>
 #include <QSslConfiguration>
@@ -214,11 +214,7 @@ void SimpleSocketPrivate::releaseSocket()
   sendBuffer.clear();
   readBuffer.clear();
   txBuffer.clear();
-  if (nextBlockSize)
-    qDebug() << "*** BUG **** nextBlockSize is not 0, size:" << nextBlockSize;
-
   nextBlockSize = 0;
-
   deliveryConfirm.clear();
 
   emit(q->released(id));
