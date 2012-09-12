f1cf1e73c1a51d2ec1a59dc7293d53d71449a8eb
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-09-12 19:09:46 +0000

    * Начало разработки версии 1.99.40.

--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -21,8 +21,8 @@
 
 #include "revision.h"
 
-#define SCHAT_VERSION      "1.99.39"
-#define SCHAT_VERSION_RC   1,99,39,SCHAT_REVISION
+#define SCHAT_VERSION      "1.99.40"
+#define SCHAT_VERSION_RC   1,99,40,SCHAT_REVISION
 #define SCHAT_NAME         "Simple Chat"
 #define SCHAT_ORGANIZATION "IMPOMEZIA"
 #define SCHAT_DOMAIN       "schat.me"
