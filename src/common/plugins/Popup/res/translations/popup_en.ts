756776ba9a6c55b8afb8ccadb7f0ef10b1ebc6d1
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-10-10 16:59:51 +0000

    + [Popup] Добавлена поддержка перевода.

---
+++ src/common/plugins/Popup/res/translations/popup_en.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>PopupSettings</name>
+    <message>
+        <location filename="../../PopupSettings.cpp" line="40"/>
+        <source>Popup windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ src/common/plugins/Popup/res/translations/popup_ru.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>PopupSettings</name>
+    <message>
+        <location filename="../../PopupSettings.cpp" line="40"/>
+        <source>Popup windows</source>
+        <translation>Всплывающие окна</translation>
+    </message>
+</context>
+</TS>
