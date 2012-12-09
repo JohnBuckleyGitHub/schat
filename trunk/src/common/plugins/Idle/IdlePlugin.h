ebcfca7d3c01aaee6c503e12d322ed69298b0623
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-12-09 15:15:50 +0000

    * [Idle] Удалён неиспользуемый код для Linux.

--- trunk/src/common/plugins/Idle/IdlePlugin.h
+++ trunk/src/common/plugins/Idle/IdlePlugin.h
@@ -37,7 +37,7 @@ public:
     QVariantMap out = CoreApi::header();
     out["Id"]           = "Idle";
     out["Name"]         = "Idle";
-    out["Version"]      = "0.2.4";
+    out["Version"]      = "0.2.5";
     out["Site"]         = "http://wiki.schat.me/Plugin/Idle";
     out["Desc"]         = "Detect idle time to automatically set Away status";
     out["Desc/ru"]      = "Обнаружение времени простоя для автоматической установки статуса Отсутствую";
