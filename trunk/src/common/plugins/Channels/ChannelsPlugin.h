9c5b07535e89433df8b17c587e1eed6bcea6d10c
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-12-11 22:06:34 +0000

    * Обновлена история версий.

--- trunk/src/common/plugins/Channels/ChannelsPlugin.h
+++ trunk/src/common/plugins/Channels/ChannelsPlugin.h
@@ -37,7 +37,7 @@ public:
     QVariantMap out     = CoreApi::header();
     out["Id"]           = "Channels";
     out["Name"]         = "Channels";
-    out["Version"]      = "0.1.0";
+    out["Version"]      = "0.1.1";
     out["Site"]         = "http://wiki.schat.me/Plugin/Channels";
     out["Desc"]         = "Advanced channels support";
     out["Desc/ru"]      = "Расширенная поддержка каналов";
