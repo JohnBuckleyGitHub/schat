67df8e234bd36e8ef8eca79ed70363bb42026aac
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2012-12-16 03:28:38 +0000

    + [NodeChannels] Добавлена поддержка установки текстового заголовка канала.

--- trunk/src/common/plugins/NodeChannels/feeds/NodeInfoFeed.cpp
+++ trunk/src/common/plugins/NodeChannels/feeds/NodeInfoFeed.cpp
@@ -16,8 +16,12 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "Channel.h"
 #include "DateTime.h"
 #include "feeds/NodeInfoFeed.h"
+#include "net/packets/Notice.h"
+#include "net/SimpleID.h"
+#include "sglobal.h"
 
 NodeInfoFeed::NodeInfoFeed(const QString &name, const QVariantMap &data)
   : Feed(name, data)
@@ -42,4 +46,77 @@ Feed* NodeInfoFeed::create(const QString &name)
 Feed* NodeInfoFeed::load(const QString &name, const QVariantMap &data)
 {
   return new NodeInfoFeed(name, data);
+}
+
+
+/*!
+ * Переопределение проверки прав доступа.
+ *
+ * Этот фид использует права доступа фида "acl".
+ */
+bool NodeInfoFeed::can(Channel *channel, Acl::ResultAcl acl) const
+{
+  if (!channel && acl != Acl::Read)
+    return false;
+
+  FeedPtr feed = channel->feed(LS("acl"), false, false);
+  if (feed)
+    return feed->can(channel, acl);
+
+  return Feed::can(channel, acl);
+}
+
+
+/*!
+ * Переопределение запроса "delete".
+ *
+ * Для этой операции требуются права на редактирование.
+ */
+FeedReply NodeInfoFeed::del(const QString &path, Channel *channel)
+{
+  if (!can(channel, Acl::Edit))
+    return Notice::Forbidden;
+
+  return Feed::del(path, channel);
+}
+
+
+FeedReply NodeInfoFeed::post(const QString &path, const QVariantMap &json, Channel *channel)
+{
+  if (!can(channel, Acl::Edit))
+    return Notice::Forbidden;
+
+  if (!channel || path.isEmpty() || path.contains(LC('*')) || !json.contains(LS("value")))
+    return Notice::BadRequest;
+
+  const QVariant& value = json[LS("value")];
+
+  // Установка текстового заголовка канала.
+  if (path == LS("title")) {
+    qint64 date = DateTime::utc();
+
+    QVariantMap data;
+    data[LS("text")]   = value.toString().left(512);
+    data[LS("author")] = SimpleID::encode(channel->id());
+    data[LS("date")]   = date;
+
+    m_data[path] = data;
+    return FeedReply(Notice::OK, date);
+  }
+
+  return Notice::BadRequest;
+}
+
+
+/*!
+ * Переопределение запроса "put".
+ *
+ * Этот фид не поддерживает такой запрос.
+ */
+FeedReply NodeInfoFeed::put(const QString &path, const QVariantMap &json, Channel *channel)
+{
+  Q_UNUSED(path)
+  Q_UNUSED(json)
+  Q_UNUSED(channel)
+  return Notice::BadRequest;
 }
