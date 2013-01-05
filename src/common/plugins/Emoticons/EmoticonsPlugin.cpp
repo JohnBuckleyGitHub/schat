9d372c53853f5d8368f62a183bf19fc7d0b20dc3
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-05 13:47:27 +0000

    * [Update] Теперь плагин включен для всех платформ.

--- src/common/plugins/Emoticons/EmoticonsPlugin.cpp
+++ src/common/plugins/Emoticons/EmoticonsPlugin.cpp
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2013 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -89,11 +89,14 @@ void EmoticonsPluginImpl::start()
 }
 
 
+bool EmoticonsPlugin::check() const
+{
+  return ChatCore::config().contains(LS("EXTENSIONS"));
+}
+
+
 ChatPlugin *EmoticonsPlugin::create()
 {
-  if (!ChatCore::config().contains(LS("EXTENSIONS")))
-    return 0;
-
   m_plugin = new EmoticonsPluginImpl(this);
   return m_plugin;
 }
