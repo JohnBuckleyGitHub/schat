8b8b16d74e9c0b7dd6c968fea0b7037b5921c6dd
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-07 17:52:45 +0000

    + [buildbot] Добавлена поддержка обновления информации для уведомления о новых версиях в OS X и Ubuntu.

--- trunk/misc/buildbot/Update.class.php
+++ trunk/misc/buildbot/Update.class.php
@@ -2,71 +2,70 @@
 
 class Update
 {
-  public $channel  = 'devel';
+  public $channel  = 'stable';
   public $version  = '';
   public $revision = 0;
-  public $os       = 'win32';
+  public $os       = '';
   public $path     = '/var/www/download.schat.me/htdocs/schat2/snapshots';
-  public $fileName = '/var/www/download.schat.me/htdocs/schat2/update.json';
-
-  private $json;
-
+  public $json     = '/var/www/download.schat.me/htdocs/schat2/update.json';
 
   /**
-   * @param array $options Опции для инициализации данных.
+   * @param $version
+   * @param $revision
+   * @param $channel
    */
-  public function __construct($options = array())
-  {
-    if (array_key_exists('channel', $options))
-      $this->channel = $options['channel'];
-
-    if (array_key_exists('version', $options))
-      $this->version = $options['version'];
-
-    if (array_key_exists('revision', $options))
-      $this->revision = $options['revision'];
-
-    if (array_key_exists('os', $options))
-      $this->os = $options['os'];
+  public function __construct($version, $revision, $channel) {
+    $this->version  = $version;
+    $this->revision = $revision;
+    $this->channel  = $channel;
   }
 
 
   /**
    * Запись информации об обновлении.
+   *
    * @return bool
    */
-  public function exec()
-  {
-    $file = $this->fileName();
-    if (!file_exists($file))
+  public function exec() {
+    $file = $this->file();
+    if ($file != '' && !file_exists($file))
       return FALSE;
 
-    $this->json = json_decode(file_get_contents($this->fileName));
+    @$json = json_decode(file_get_contents($this->json));
 
-    $this->json->{$this->channel}->{$this->os}->version  = $this->version;
-    $this->json->{$this->channel}->{$this->os}->revision = intval($this->revision);
-    $this->json->{$this->channel}->{$this->os}->file     = $this->fileUrl();
-    $this->json->{$this->channel}->{$this->os}->size     = filesize($file);
-    $this->json->{$this->channel}->{$this->os}->hash     = sha1_file($file);
+    $json->{$this->channel}->{$this->os}->version  = $this->version;
+    $json->{$this->channel}->{$this->os}->revision = intval($this->revision);
 
-    return $this->write();
+    if ($file != '') {
+      $json->{$this->channel}->{$this->os}->file   = $this->url();
+      $json->{$this->channel}->{$this->os}->size   = filesize($file);
+      $json->{$this->channel}->{$this->os}->hash   = sha1_file($file);
+    }
+
+    $page = $this->page();
+    if ($page != '')
+      $json->{$this->channel}->{$this->os}->page   = $page;
+
+    return $this->write($json);
   }
 
 
   /**
    * Запись JSON в файл.
+   *
+   * @param $json Объект для записи в файл.
    * @return bool
    */
-  private function write()
+  protected final function write($json)
   {
-    $result = json_encode($this->json);
+    $result = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     if ($result === FALSE) {
       echo 'json encode failed' . json_last_error() . "\n";
       return FALSE;
     }
 
-    if (file_put_contents($this->fileName, $result) === FALSE) {
-      echo 'write ' . $this->fileName . " failed\n";
+    if (file_put_contents($this->json, $result) === FALSE) {
+      echo 'write ' . $this->json . " failed\n";
       return FALSE;
     }
 
@@ -77,21 +76,78 @@ class Update
   /**
    * @return string Возвращает локальное имя файла обновления.
    */
-  private function fileName()
-  {
-    return $this->path . '/' . $this->version . '/r' . $this->revision . '/schat2-' . $this->version . '-dev.exe';
-  }
+  protected function file() { return ''; }
 
 
   /**
    * @return string Возвращает публичный адрес файла обновления.
    */
-  private function fileUrl()
-  {
+  protected function url() { return ''; }
+
+
+  /**
+   * @return string Адрес страницы для загрузки обновления.
+   */
+  protected function page() { return ''; }
+}
+
+
+class UpdateWin32 extends Update
+{
+  public function __construct($version, $revision, $channel) {
+    parent::__construct($version, $revision, $channel);
+    $this->os = 'win32';
+  }
+
+
+  protected function file() {
+    return $this->path . '/' . $this->version . '/r' . $this->revision . '/schat2-' . $this->version . '-dev.exe';
+  }
+
+
+  protected function url() {
     if ($this->channel == 'stable')
       return 'http://impomezia.s3.amazonaws.com/schat/' . $this->version . '/schat2-' . $this->version . '.exe';
     else
       return 'http://download.schat.me/schat2/snapshots/' . $this->version . '/r' . $this->revision . '/schat2-' . $this->version . '-dev.exe';
+  }
+
+
+  protected function page() {
+    return 'https://schat.me/download/windows';
+  }
+}
+
+
+class UpdateOSX extends Update
+{
+  public function __construct($version, $revision, $channel) {
+    parent::__construct($version, $revision, $channel);
+    $this->os = 'osx';
+  }
+
+
+  protected function file() {
+    return $this->path . '/' . $this->version . '/r' . $this->revision . '/SimpleChat2-' . $this->version . '-dev.dmg';
+  }
+
+
+  protected function url() {
+    return 'http://impomezia.s3.amazonaws.com/schat/' . $this->version . '/SimpleChat2-' . $this->version . '.dmg';
+  }
+}
+
+
+class UpdateUbuntu extends Update
+{
+  public function __construct($version, $revision, $channel) {
+    parent::__construct($version, $revision, $channel);
+    $this->os = 'ubuntu';
+  }
+
+
+  protected function page() {
+    return 'https://schat.me/download/ubuntu';
   }
 }
 
