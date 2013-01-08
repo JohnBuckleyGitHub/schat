cb44d81a9c7f043bfc25889de4c21c314a78ca35
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-08 19:04:00 +0000

    * Исправлено падение при выходе, если были открыты вкладки с непрочитанными сообщениями.

--- trunk/src/schat2/ChatAlerts.h
+++ trunk/src/schat2/ChatAlerts.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2013 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -83,6 +83,7 @@ public:
   static void play(const QString &file);
   static void remove(const QByteArray &channelId);
   static void remove(const QByteArray &channelId, const QByteArray &alertId);
+  static void reset();
 
 signals:
   void alert(bool alert);
