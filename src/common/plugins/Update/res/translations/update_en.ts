166797e70f17d334f5ba3037bc61ba2a49b79cbb
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-08 14:13:16 +0000

    * [Update] Исправлено сообщение о доступности новой версии в Ubuntu, т.к. не известен размер обновлений. * [Popup] Используется Qt::WA_ShowWithoutActivating.

--- src/common/plugins/Update/res/translations/update_en.ts
+++ src/common/plugins/Update/res/translations/update_en.ts
@@ -5,42 +5,42 @@
 <context>
     <name>UpdatePluginImpl</name>
     <message>
-        <location filename="../../UpdatePlugin.cpp" line="200"/>
+        <location filename="../../UpdatePlugin.cpp" line="202"/>
         <source>Release Notes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdatePlugin.cpp" line="206"/>
+        <location filename="../../UpdatePlugin.cpp" line="208"/>
         <source>Download</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdatePlugin.cpp" line="209"/>
+        <location filename="../../UpdatePlugin.cpp" line="211"/>
         <source>Pause</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdatePlugin.cpp" line="212"/>
+        <location filename="../../UpdatePlugin.cpp" line="214"/>
         <source>Resume</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdatePlugin.cpp" line="253"/>
+        <location filename="../../UpdatePlugin.cpp" line="255"/>
         <source>Downloading update</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdatePlugin.cpp" line="433"/>
+        <location filename="../../UpdatePlugin.cpp" line="432"/>
         <source>Install Update Now</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdatePlugin.cpp" line="438"/>
+        <location filename="../../UpdatePlugin.cpp" line="437"/>
         <source>Update Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdatePlugin.cpp" line="442"/>
+        <location filename="../../UpdatePlugin.cpp" line="441"/>
         <source>Update Available</source>
         <translation type="unfinished"></translation>
     </message>
@@ -56,37 +56,42 @@
 <context>
     <name>UpdateWidget</name>
     <message>
-        <location filename="../../UpdateWidget.cpp" line="59"/>
+        <location filename="../../UpdateWidget.cpp" line="56"/>
         <source>Checking for updates...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdateWidget.cpp" line="74"/>
+        <location filename="../../UpdateWidget.cpp" line="70"/>
         <source>An error occurred while checking for updates.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdateWidget.cpp" line="79"/>
+        <location filename="../../UpdateWidget.cpp" line="75"/>
         <source>Simple Chat is up to date.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdateWidget.cpp" line="85"/>
+        <location filename="../../UpdateWidget.cpp" line="81"/>
         <source>Downloading update...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdateWidget.cpp" line="90"/>
+        <location filename="../../UpdateWidget.cpp" line="87"/>
         <source>New version of Simple Chat available. Version &lt;b&gt;%1&lt;/b&gt;, size &lt;b&gt;%2&lt;/b&gt;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdateWidget.cpp" line="97"/>
+        <location filename="../../UpdateWidget.cpp" line="89"/>
+        <source>New version of Simple Chat available. Version &lt;b&gt;%1&lt;/b&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../UpdateWidget.cpp" line="95"/>
         <source>An error occurred when downloading updates.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UpdateWidget.cpp" line="102"/>
+        <location filename="../../UpdateWidget.cpp" line="100"/>
         <source>Update ready to install.</source>
         <translation type="unfinished"></translation>
     </message>
