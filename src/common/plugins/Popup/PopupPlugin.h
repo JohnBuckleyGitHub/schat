4056147aaa494770688de29caa5f4a64aa6cc688
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-08 20:23:55 +0000

    * Обновлена история версий.

--- src/common/plugins/Popup/PopupPlugin.h
+++ src/common/plugins/Popup/PopupPlugin.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2013 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -37,7 +37,7 @@ public:
     QVariantMap out = CoreApi::header();
     out["Id"]       = "Popup";
     out["Name"]     = "Popup";
-    out["Version"]  = "0.1.1";
+    out["Version"]  = "0.1.2";
     out["Site"]     = "http://wiki.schat.me/Plugin/Popup";
     out["Desc"]     = "Adds support for popup notifications";
     out["Desc/ru"]  = "Добавляет поддержку всплывающих уведомлений";
