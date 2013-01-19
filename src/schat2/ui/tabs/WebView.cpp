bcdefb466b1487699a3dc3536fb84dce7ec8e0af
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-19 06:36:07 +0000

    * Исправлен избыточный запрос фида "server". * Восстановлена поддержка Qt 5.

--- src/schat2/ui/tabs/WebView.cpp
+++ src/schat2/ui/tabs/WebView.cpp
@@ -16,11 +16,12 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QContextMenuEvent>
 #include <QAction>
 #include <QApplication>
 #include <QClipboard>
+#include <QContextMenuEvent>
 #include <QMenu>
+#include <QMimeData>
 #include <QWebFrame>
 
 #include "ui/tabs/WebView.h"
