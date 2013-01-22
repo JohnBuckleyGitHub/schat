0e02ac00a02f02ff43cbec5f7701a650a2bb70ac
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-22 18:10:36 +0000

    + Добавлено импортирование существующих прав доступа в фид "acl".

--- src/schatd2/feeds/NodeAclFeed.cpp
+++ src/schatd2/feeds/NodeAclFeed.cpp
@@ -17,6 +17,7 @@
  */
 
 #include "DateTime.h"
+#include "acl/AclValue.h"
 #include "feeds/NodeAclFeed.h"
 #include "net/packets/Notice.h"
 #include "sglobal.h"
@@ -24,13 +25,14 @@
 NodeAclFeed::NodeAclFeed(const QString &name, const QVariantMap &data)
   : Feed(name, data)
 {
+  init();
 }
 
 
 NodeAclFeed::NodeAclFeed(const QString &name, qint64 date)
   : Feed(name, date)
 {
-  m_header.acl().setMask(0766);
+  init();
 }
 
 
@@ -58,6 +60,33 @@ FeedReply NodeAclFeed::put(const QString &path, const QVariantMap &json, Channel
     return Feed::put(path, json, channel);
 
   return Notice::Forbidden;
+}
+
+
+void NodeAclFeed::init()
+{
+  if (!m_data.isEmpty())
+    return;
+
+  // Импортирование существующих прав доступа.
+  const QVariantMap &data = head().data();
+  m_data[FEED_WILDCARD_ASTERISK] = AclValue::toByteArray(head().acl().mask() & ~0770);
+
+  const QVariantMap others = data.value(ACL_OTHERS).toMap();
+  if (!others.isEmpty()) {
+    QMapIterator<QString, QVariant> i(others);
+    while (i.hasNext()) {
+      i.next();
+      m_data[i.key()] = AclValue::toByteArray(i.value().toInt());
+    }
+  }
+
+  const QStringList owners = data.value(ACL_OWNERS).toStringList();
+  if (!owners.isEmpty()) {
+    const QString mask = AclValue::toByteArray(head().acl().mask() >> 6 | Acl::SpecialEdit);
+    foreach (const QString &id, owners)
+      m_data[id] = mask;
+  }
 }
 
 
