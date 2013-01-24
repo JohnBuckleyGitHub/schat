7594839859c749f2246ca40458debf9693189505
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-24 20:52:46 +0000

    + Реализована основная функциональность фида "users".

--- trunk/src/common/plugins/NodeChannels/feeds/NodeUsersFeed.cpp
+++ trunk/src/common/plugins/NodeChannels/feeds/NodeUsersFeed.cpp
@@ -16,20 +16,67 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "Channel.h"
 #include "DateTime.h"
 #include "feeds/NodeUsersFeed.h"
+#include "net/packets/Notice.h"
+#include "net/SimpleID.h"
+#include "sglobal.h"
 
 NodeUsersFeed::NodeUsersFeed(const QString &name, const QVariantMap &data)
   : Feed(name, data)
 {
-  m_header.acl().setMask(0444);
+  init();
 }
 
 
 NodeUsersFeed::NodeUsersFeed(const QString &name, qint64 date)
   : Feed(name, date)
 {
+  init();
+}
+
+
+FeedReply NodeUsersFeed::del(const QString &path, Channel *channel)
+{
+  if (!can(channel, Acl::Edit | Acl::SpecialEdit))
+    return Notice::Forbidden;
+
+  if (path.size() == 34) {
+    int count = m_data.value(LS("count")).toInt();
+    if (SimpleID::typeOf(SimpleID::decode(path)) == SimpleID::UserId) {
+      --count;
+      m_data[LS("count")] = count;
+      return FeedReply(Notice::OK, DateTime::utc());
+    }
+  }
+
+  return Notice::NotModified;
+}
+
+
+FeedReply NodeUsersFeed::post(const QString &path, const QVariantMap &json, Channel *channel)
+{
+  if (!can(channel, Acl::Edit | Acl::SpecialEdit))
+    return Notice::Forbidden;
+
+  if (path.size() == 34) {
+    int count = m_data.value(LS("count")).toInt();
+    if (SimpleID::typeOf(SimpleID::decode(path)) == SimpleID::UserId) {
+      ++count;
+      m_data[LS("count")] = count;
+      return FeedReply(Notice::OK, DateTime::utc());
+    }
+  }
+
+  return Notice::NotModified;
+}
+
+
+void NodeUsersFeed::init()
+{
   m_header.acl().setMask(0444);
+  m_data[LS("count")] = 0;
 }
 
 
