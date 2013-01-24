d23ddde87f8050bcb6551069a54439d5bc18a7c9
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-24 20:52:46 +0000

    + Реализована основная функциональность фида "users".

--- src/common/plugins/NodeChannels/feeds/NodeUsersFeed.h
+++ src/common/plugins/NodeChannels/feeds/NodeUsersFeed.h
@@ -30,6 +30,12 @@ class NodeUsersFeed : public Feed
 public:
   NodeUsersFeed(const QString &name, const QVariantMap &data);
   NodeUsersFeed(const QString &name = FEED_NAME_USERS, qint64 date = 0);
+
+  FeedReply del(const QString &path, Channel *channel = 0);
+  FeedReply post(const QString &path, const QVariantMap &json, Channel *channel = 0);
+
+private:
+  void init();
 };
 
 
