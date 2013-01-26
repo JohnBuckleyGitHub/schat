57b4bda1d9e12856da5a617526f9fed59e61b1af
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-01-26 14:54:02 +0000

    * Исправлена избыточная загрузка фидов.

--- src/common/client/ClientHooks.h
+++ src/common/client/ClientHooks.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2013 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -116,7 +116,8 @@ protected:
   virtual void addImpl(ClientChannel channel, const ChannelInfo &info, const QVariantMap &json);
   virtual void readFeedImpl(const FeedNotice &packet);
 
-  QList<Feeds*> m_hooks; ///< Хуки.
+  QList<Feeds*> m_hooks;            ///< Хуки.
+  QMap<QByteArray, qint64> m_cache; ///< Блокирующий кэш препятствующий двойной загрузке фидов.
 };
 
 }  // namespace Hooks
