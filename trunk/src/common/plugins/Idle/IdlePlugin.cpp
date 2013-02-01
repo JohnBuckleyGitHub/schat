031cbf6eb0327b1ed2b8408893af008553c9b213
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-02-01 22:06:52 +0000

    * Обновлена история версий.

--- trunk/src/common/plugins/Idle/IdlePlugin.cpp
+++ trunk/src/common/plugins/Idle/IdlePlugin.cpp
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2013 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -92,6 +92,9 @@ void IdlePluginImpl::away(bool away)
   int status = Status::AutoAway;
   if (!away)
     status = ChatCore::settings()->value(LS("Profile/Status")).toInt();
+
+  if (status == Status::Offline)
+    status = Status::Online;
 
   ChatClient::channel()->status() = status;
   ChatClient::channels()->update();
