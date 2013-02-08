a9f13dbbba61bc8fa21109531aa69f0208909da5
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-02-08 12:12:10 +0000

    * Обновлена история версий.

--- trunk/src/common/plugins/NodeConsole/NodeConsolePlugin.h
+++ trunk/src/common/plugins/NodeConsole/NodeConsolePlugin.h
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2013 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -37,7 +37,7 @@ public:
     QVariantMap out        = CoreApi::header();
     out[CORE_API_ID]       = "NodeConsole";
     out[CORE_API_NAME]     = "Node Console";
-    out[CORE_API_VERSION]  = "0.1.1";
+    out[CORE_API_VERSION]  = "0.1.2";
     out[CORE_API_TYPE]     = "server";
     out[CORE_API_SITE]     = "http://wiki.schat.me/Plugin/NodeConsole";
     out[CORE_API_DESC]     = "Node Console";
