c4cab29d730d1a728fcc7b7f83bcb2d8ca859bb6
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-02-16 10:13:00 +0000

    * Начало разработки 2.0.0.

--- trunk/src/common/common.pri
+++ trunk/src/common/common.pri
@@ -30,7 +30,7 @@ INCLUDEPATH += \
     ../common \
     ../3rdparty \
 
-!win32:VERSION = 1.99.56
+!win32:VERSION = 2.0.0
 
 CONFIG(debug, debug|release) { 
   RCC_DIR = ../../tmp/$${TEMPLATE}/$${TARGET}/debug/rcc
--- trunk/src/common/version.h
+++ trunk/src/common/version.h
@@ -21,8 +21,8 @@
 
 #include "revision.h"
 
-#define SCHAT_VERSION      "1.99.56"
-#define SCHAT_VERSION_RC   1,99,56,SCHAT_REVISION
+#define SCHAT_VERSION      "2.0.0"
+#define SCHAT_VERSION_RC   2,0,0,SCHAT_REVISION
 #define SCHAT_NAME         "Simple Chat"
 #define SCHAT_ORGANIZATION "Alexander Sedov"
 #define SCHAT_DOMAIN       "schat.me"
