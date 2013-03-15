de27dcbfe4defde5fea661020a4c5224de615456
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-03-15 12:53:36 +0000

    * Исправлена ошибка при генерации уникального идентификатора компьютера.

--- src/common/client/AbstractClient.cpp
+++ src/common/client/AbstractClient.cpp
@@ -44,7 +44,6 @@ AbstractClientPrivate::AbstractClientPrivate()
   , reconnects(0)
   , pool(new NetworkPool())
   , reconnectTimer(new QBasicTimer())
-  , uniqueId(SimpleID::uniqueId(Channel::defaultName().toUtf8()))
 {
   Account account;
   channel->setAccount(&account);
--- src/common/client/SimpleClient.cpp
+++ src/common/client/SimpleClient.cpp
@@ -142,6 +142,9 @@ void SimpleClient::requestAuth()
   if (d->authType == AuthRequest::Cookie && SimpleID::typeOf(d->cookie) != SimpleID::CookieId)
     d->authType = AuthRequest::Discovery;
 
+  if (d->uniqueId.isEmpty())
+    d->uniqueId = SimpleID::uniqueId(Channel::defaultName().toUtf8());
+
   AuthRequest data(d->authType, d->url.toString(), d->channel.data());
   data.uniqueId = d->uniqueId;
   data.cookie   = d->cookie;
