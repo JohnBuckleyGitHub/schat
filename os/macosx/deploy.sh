7bd06566563750a0a5ccb8681fde371d5ceebe4b
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-03-19 20:58:34 +0000

    * Начало разработки 2.0.4.

--- os/macosx/deploy.sh
+++ os/macosx/deploy.sh
@@ -16,7 +16,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-VERSION="2.0.3"
+VERSION="2.0.4"
 TARGET="SimpleChat2"
 
 set -e
