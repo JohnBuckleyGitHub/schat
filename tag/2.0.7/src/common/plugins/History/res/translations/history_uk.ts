9522cda0f1bc471f7e6e76ffd8079c7d97eb3649
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-04-25 00:19:37 +0000

    * TAG 2.0.7.

---
+++ tag/2.0.7/src/common/plugins/History/res/translations/history_uk.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk_UA">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>HistoryAction</name>
+    <message>
+        <location filename="../../HistoryButton.cpp" line="163"/>
+        <source>History</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HistoryButton</name>
+    <message>
+        <location filename="../../HistoryButton.cpp" line="128"/>
+        <source>History</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HistorySettings</name>
+    <message>
+        <location filename="../../ui/HistorySettings.cpp" line="60"/>
+        <source>Automatically display last 20 private messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ProfilePluginTr</name>
+    <message>
+        <location filename="../../HistoryPlugin.cpp" line="56"/>
+        <source>Loading messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../HistoryPlugin.cpp" line="57"/>
+        <source>More messages</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
