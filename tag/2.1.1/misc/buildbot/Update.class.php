c638681cae9f8e7144b8967e4a02032be9b4fce5
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-08-25 01:49:47 +0000

    * TAG 2.1.1.

---
+++ tag/2.1.1/misc/buildbot/Update.class.php
@@ -0,0 +1,152 @@
+<?php
+
+class Update
+{
+  public $channel  = 'stable';
+  public $version  = '';
+  public $revision = 0;
+  public $os       = '';
+  public $path     = '/var/www/download.schat.me/htdocs/schat2/snapshots';
+  public $json     = '/var/www/download.schat.me/htdocs/schat2/update.json';
+
+  /**
+   * @param $version
+   * @param $revision
+   * @param $channel
+   */
+  public function __construct($version, $revision, $channel) {
+    $this->version  = $version;
+    $this->revision = $revision;
+    $this->channel  = $channel;
+  }
+
+
+  /**
+   * Запись информации об обновлении.
+   *
+   * @return bool
+   */
+  public function exec() {
+    $file = $this->file();
+    if ($file != '' && !file_exists($file))
+      return FALSE;
+
+    @$json = json_decode(file_get_contents($this->json));
+
+    $json->{$this->channel}->{$this->os}->version  = $this->version;
+    $json->{$this->channel}->{$this->os}->revision = intval($this->revision);
+
+    if ($file != '') {
+      $json->{$this->channel}->{$this->os}->file   = $this->url();
+      $json->{$this->channel}->{$this->os}->size   = filesize($file);
+      $json->{$this->channel}->{$this->os}->hash   = sha1_file($file);
+    }
+
+    $page = $this->page();
+    if ($page != '')
+      $json->{$this->channel}->{$this->os}->page   = $page;
+
+    return $this->write($json);
+  }
+
+
+  /**
+   * Запись JSON в файл.
+   *
+   * @param $json Объект для записи в файл.
+   * @return bool
+   */
+  protected final function write($json)
+  {
+    $result = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    if ($result === FALSE) {
+      echo 'json encode failed' . json_last_error() . "\n";
+      return FALSE;
+    }
+
+    if (file_put_contents($this->json, $result) === FALSE) {
+      echo 'write ' . $this->json . " failed\n";
+      return FALSE;
+    }
+
+    return TRUE;
+  }
+
+
+  /**
+   * @return string Возвращает локальное имя файла обновления.
+   */
+  protected function file() { return ''; }
+
+
+  /**
+   * @return string Возвращает публичный адрес файла обновления.
+   */
+  protected function url() { return ''; }
+
+
+  /**
+   * @return string Адрес страницы для загрузки обновления.
+   */
+  protected function page() { return ''; }
+}
+
+
+class UpdateWin32 extends Update
+{
+  public function __construct($version, $revision, $channel) {
+    parent::__construct($version, $revision, $channel);
+    $this->os = 'win32';
+  }
+
+
+  protected function file() {
+    return $this->path . '/' . $this->version . '/r' . $this->revision . '/schat2-' . $this->version . '-dev.exe';
+  }
+
+
+  protected function url() {
+    if ($this->channel == 'stable')
+      return 'http://schat.googlecode.com/files/schat2-' . $this->version . '.exe';
+    else
+      return 'https://download.schat.me/schat2/snapshots/' . $this->version . '/r' . $this->revision . '/schat2-' . $this->version . '-dev.exe';
+  }
+
+
+  protected function page() {
+    return 'https://schat.me/download/windows';
+  }
+}
+
+
+class UpdateOSX extends Update
+{
+  public function __construct($version, $revision, $channel) {
+    parent::__construct($version, $revision, $channel);
+    $this->os = 'osx';
+  }
+
+
+  protected function file() {
+    return $this->path . '/' . $this->version . '/r' . $this->revision . '/SimpleChat2-' . $this->version . '-dev.dmg';
+  }
+
+
+  protected function url() {
+    return 'http://schat.googlecode.com/files/SimpleChat2-' . $this->version . '.dmg';
+  }
+}
+
+
+class UpdateUbuntu extends Update
+{
+  public function __construct($version, $revision, $channel) {
+    parent::__construct($version, $revision, $channel);
+    $this->os = 'ubuntu';
+  }
+
+
+  protected function page() {
+    return 'https://schat.me/download/ubuntu';
+  }
+}
---
+++ tag/2.1.1/misc/tools/yui.cmd
@@ -0,0 +1,52 @@
+@echo off
+java -jar yuicompressor.jar ..\..\res\html\src\ChatView.js -o ..\..\res\html\js\ChatView.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\PluginsView.js -o ..\..\res\html\js\PluginsView.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\server.js -o ..\..\res\html\js\server.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\user.js -o ..\..\res\html\js\user.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\about.js -o ..\..\res\html\js\about.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\bootstrap-tooltip.js -o ..\..\res\html\js\bootstrap-tooltip.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\bootstrap-modal.js -o ..\..\res\html\js\bootstrap-modal.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\jquery.timeago.js -o ..\..\res\html\js\jquery.timeago.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\jquery.timeago.en.js -o ..\..\res\html\js\jquery.timeago.en.min.js
+java -jar yuicompressor.jar ..\..\res\html\src\jquery.timeago.ru.js -o ..\..\res\html\js\jquery.timeago.ru.min.js
+
+java -jar yuicompressor.jar ..\..\res\html\src\about.css -o ..\..\res\html\css\about.min.css
+java -jar yuicompressor.jar ..\..\res\html\src\PluginsView.css -o ..\..\res\html\css\PluginsView.min.css
+java -jar yuicompressor.jar ..\..\res\html\src\ChatView.css -o ..\..\res\html\css\ChatView.min.css
+java -jar yuicompressor.jar ..\..\res\html\src\server.css -o ..\..\res\html\css\server.min.css
+java -jar yuicompressor.jar ..\..\res\html\src\user.css -o ..\..\res\html\css\user.min.css
+java -jar yuicompressor.jar ..\..\res\html\src\channel.css -o ..\..\res\html\css\channel.min.css
+
+java -jar yuicompressor.jar ..\..\src\common\plugins\RawFeeds\res\src\RawFeeds.js -o ..\..\src\common\plugins\RawFeeds\res\js\RawFeeds.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\RawFeeds\res\src\KelpJSONView.js -o ..\..\src\common\plugins\RawFeeds\res\js\KelpJSONView.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\RawFeeds\res\src\RawFeeds.css -o ..\..\src\common\plugins\RawFeeds\res\css\RawFeeds.min.css
+
+java -jar yuicompressor.jar ..\..\src\common\plugins\Profile\res\src\Profile.js -o ..\..\src\common\plugins\Profile\res\js\Profile.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Profile\res\src\flags.css -o ..\..\src\common\plugins\Profile\res\css\flags.min.css
+
+java -jar yuicompressor.jar ..\..\src\common\plugins\YouTube\res\src\YouTube.js -o ..\..\src\common\plugins\YouTube\res\js\YouTube.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\YouTube\res\src\YouTube.css -o ..\..\src\common\plugins\YouTube\res\css\YouTube.min.css
+
+java -jar yuicompressor.jar ..\..\src\common\plugins\SendFile\res\src\SendFile.js -o ..\..\src\common\plugins\SendFile\res\js\SendFile.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\SendFile\res\src\SendFile.css -o ..\..\src\common\plugins\SendFile\res\css\SendFile.min.css
+
+java -jar yuicompressor.jar ..\..\src\common\plugins\History\res\src\History.js -o ..\..\src\common\plugins\History\res\js\History.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\History\res\src\days.js -o ..\..\src\common\plugins\History\res\js\days.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\History\res\src\History.css -o ..\..\src\common\plugins\History\res\css\History.min.css
+
+java -jar yuicompressor.jar ..\..\src\common\plugins\Console\res\html\src\console.js -o ..\..\src\common\plugins\Console\res\js\console.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Console\res\html\src\console-login.js -o ..\..\src\common\plugins\Console\res\js\console-login.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Console\res\html\src\console-password.js -o ..\..\src\common\plugins\Console\res\js\console-password.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Console\res\html\src\console-home.js -o ..\..\src\common\plugins\Console\res\js\console-home.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Console\res\html\src\console.css -o ..\..\src\common\plugins\Console\res\css\console.min.css
+
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\bootstrap-dropdown.js -o ..\..\src\common\plugins\Channels\res\js\bootstrap-dropdown.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\Channels.js -o ..\..\src\common\plugins\Channels\res\js\Channels.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\ChannelsUser.js -o ..\..\src\common\plugins\Channels\res\js\ChannelsUser.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\ListView.js -o ..\..\src\common\plugins\Channels\res\js\ListView.min.js
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\bootstrap.css -o ..\..\src\common\plugins\Channels\res\css\bootstrap.min.css
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\bootstrap-dropdown.css -o ..\..\src\common\plugins\Channels\res\css\bootstrap-dropdown.min.css
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\bootstrap-override.css -o ..\..\src\common\plugins\Channels\res\css\bootstrap-override.min.css
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\Channels.css -o ..\..\src\common\plugins\Channels\res\css\Channels.min.css
+java -jar yuicompressor.jar ..\..\src\common\plugins\Channels\res\src\ListView.css -o ..\..\src\common\plugins\Channels\res\css\ListView.min.css
+pause
