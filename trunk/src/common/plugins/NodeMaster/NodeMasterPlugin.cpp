004a7937d99b07b55ccd24deec6f242c4151ba9b
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-08-30 17:33:43 +0000

    + Добавлен базовый каркас плагина NodeMaster.

---
+++ trunk/src/common/plugins/NodeMaster/NodeMasterPlugin.cpp
@@ -0,0 +1,38 @@
+/* $Id$
+ * IMPOMEZIA Simple Chat
+ * Copyright © 2008-2013 IMPOMEZIA <schat@impomezia.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtPlugin>
+
+#include "NodeMasterPlugin.h"
+#include "NodeMasterPlugin_p.h"
+
+NodeMasterImpl::NodeMasterImpl(QObject *parent)
+  : NodePlugin(parent)
+{
+}
+
+
+NodePlugin *NodeMasterPlugin::create()
+{
+  m_plugin = new NodeMasterImpl(this);
+  return m_plugin;
+}
+
+#if QT_VERSION < 0x050000
+  Q_EXPORT_PLUGIN2(NodeMaster, NodeMasterPlugin);
+#endif
