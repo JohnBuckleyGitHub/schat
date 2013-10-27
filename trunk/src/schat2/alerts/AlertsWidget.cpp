0c42703be0ec65c0516fd6145c4f422983ffdc59
IMPOMEZIA <IMPOMEZIA@31f87582-d853-11dd-87b6-9d1a99559f42>
2013-10-27 03:39:07 +0000

    * Исправлено, звуковые уведомления для событий "в сети" и "не в сети" не работали.

--- trunk/src/schat2/alerts/AlertsWidget.cpp
+++ trunk/src/schat2/alerts/AlertsWidget.cpp
@@ -1,6 +1,6 @@
 /* $Id$
  * IMPOMEZIA Simple Chat
- * Copyright © 2008-2012 IMPOMEZIA <schat@impomezia.com>
+ * Copyright © 2008-2013 IMPOMEZIA <schat@impomezia.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -133,7 +133,7 @@ void AlertsWidget::indexChanged(int index)
   m_popup->setVisible(supports.contains(LS("popup")));
   setSoundVisible(supports.contains(LS("sound")));
 
-  if (m_sound->isChecked() && m_toolBar->isVisible())
+  if (m_sound->isChecked())
     m_file->setCurrentIndex(m_file->findText(type->value(LS("file")).toString()));
 }
 
